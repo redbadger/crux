4c762784d29556a34f2779b8ce89026ce4c7d578
Viktor Charypar <charypar@gmail.com>
2022-11-11 13:48:16 +0000

    Add TS and load the WASM core

---
+++ web-nextjs/pages/index.tsx
@@ -0,0 +1,116 @@
+import { countReset } from "console";
+import type { NextPage } from "next";
+import dynamic from "next/dynamic";
+import Head from "next/head";
+import { useEffect, useReducer } from "react";
+
+enum Action {
+  Init,
+  Clear,
+  Get,
+  Fetch,
+}
+
+type Message = {
+  action: Action;
+  core?: any;
+};
+
+type State = {
+  core?: any;
+  image: {
+    file: string;
+  };
+  fact: string;
+  platform: string;
+};
+
+const initialState = {
+  image: { file: "" },
+  fact: "",
+  platform: "",
+};
+
+function reducer(state: State, message: Message) {
+  switch (message.action) {
+    case Action.Init:
+      return { core: message.core, ...state };
+    case Action.Get:
+    case Action.Fetch:
+    case Action.Clear:
+      break;
+    default:
+      throw new Error();
+  }
+
+  return state;
+}
+
+const Home: NextPage = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    // Seems a bad idea.
+    async function loadCore() {
+      const core = await import("../pkg/shared");
+      dispatch({ action: Action.Init, core: core });
+    }
+
+    if (!state.core) {
+      loadCore();
+    }
+  });
+
+  return (
+    <div className="container">
+      <Head>
+        <title>Cat Facts - NextJS</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css"
+        />
+      </Head>
+
+      <main>
+        {!state.core ? (
+          <p></p>
+        ) : (
+          <>
+            <section className="section title has-text-centered">
+              <p>{state.platform}</p>
+            </section>
+            <section className="section container has-text-centered">
+              <img src={state.image.file} style={{ height: "400px" }} />
+            </section>
+            <section className="section container has-text-centered">
+              <p>{state.fact}</p>
+            </section>
+            <div className="buttons container is-centered">
+              <button
+                className="button is-primary is-danger"
+                onClick={() => dispatch({ action: Action.Clear })}
+              >
+                {"Clear"}
+              </button>
+              <button
+                className="button is-primary is-success"
+                onClick={() => dispatch({ action: Action.Get })}
+              >
+                {"Get"}
+              </button>
+              <button
+                className="button is-primary is-warning"
+                onClick={() => dispatch({ action: Action.Fetch })}
+              >
+                {"Fetch"}
+              </button>
+            </div>
+          </>
+        )}
+      </main>
+    </div>
+  );
+};
+
+export default Home;
