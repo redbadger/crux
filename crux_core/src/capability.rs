d4ebe8e21633acea288c2af18d8b095610f3a3d4
Viktor Charypar <charypar@gmail.com>
2022-12-15 11:38:33 +0000

    Better capability examples, including building one

--- crux_core/src/capability.rs
+++ crux_core/src/capability.rs
@@ -31,24 +31,58 @@
 //! In order to use a capability, the app needs to include it in its `Capabilities` associated type and `WithContext`
 //! trait implementation. For example:
 //!
-//! ```rust,ignore
-//! use crux_http::{Http, HttpRequest};
+//! ```rust
+//! mod root {
 //!
-//! enum Effect {
-//!     Http(HttpRequest)
+//! // An app module which can be reused in different apps
+//! mod my_app {
+//!     use crux_core::App;
+//!     use crux_core::render::{Render};
+//!     
+//!     #[derive(Default)]
+//!     pub struct MyApp;
+//!     pub struct Event;
+//!    
+//!     pub struct Capabilities {
+//!         pub render: Render<Event>
+//!     }
+//!     
+//!     impl App for MyApp {
+//!         type Model = ();
+//!         type Event = Event;
+//!         type ViewModel = ();
+//!         type Capabilities = Capabilities;
+//!    
+//!         fn update(&self, event: Event, model: &mut (), caps: &Capabilities) {
+//!             caps.render.render();
+//!         }
+//!     
+//!         fn view(&self, model: &()) {
+//!             ()    
+//!         }
+//!     }
 //! }
 //!
-//! #[derive(Clone, Serialize, Deserialize, Debug, PartialEq, Eq)]
-//! struct Capabilities {
-//!     http: Http<Effect>
-//! }
+//! // Specific app core using the my_app module
+//! mod core {
+//!     use serde::{Serialize, Deserialize};
+//!     use crux_core::capability::{CapabilityContext};
+//!     use crux_core::render::{Render};
+//!     use super::my_app::{MyApp, Event, Capabilities};
 //!
-//! impl crux_core::WithContext<App, Effect> for Capabilities {
-//!    fn new_with_context(context: CapabilityContext<Effect, Event>) -> Capabilities {
-//!        Capabilities {
-//!            http: Http::new(context.with_effect(Effect::Http)),
+//!     #[derive(Serialize, Deserialize)]
+//!     enum Effect {
+//!         Render
+//!     }
+//!    
+//!     impl crux_core::WithContext<MyApp, Effect> for Capabilities {
+//!        fn new_with_context(context: CapabilityContext<Effect, Event>) -> Capabilities {
+//!            Capabilities {
+//!                render: Render::new(context.with_effect(|_| Effect::Render)),
+//!            }
 //!        }
-//!    }
+//!     }
+//! }
 //! }
 //! ```
 //!
@@ -58,7 +92,96 @@
 //!
 //! # Implementing a capability
 //!
-//! TODO. See the crux_http crate as a working example.
+//! Capabilities provide an interface to request side-effects. The interface has asynchronous semantics
+//! with a form of callback. A typical capability call can look like this:
+//!
+//! ```rust,ignore
+//! caps.ducks.get_in_a_row(10, Event::RowOfDucks)
+//! ```
+//!
+//! The call above translates into "Get 10 ducks in a row and return them to me using the `RowOfDucks` event".
+//! The capability's job is to translate this request into serialisable message and instruct the Shell to
+//! do the duck herding and when it receives the ducks back, wrap them in the requested event and return
+//! to the app.
+//!
+//! We will refer to `get_in_row` in the above call as an _operation_, the `10` is an _input_, and the
+//! `Event::RowOfDucks` is an event constructor - a function, which eventually receives the row of ducks
+//! and returns a variant of the `Event` enum. Conveniently, enum variants are functions, and so that
+//! will be the typical use.
+//!
+//! This is what the capability implementation could look like:
+//!
+//! ```rust
+//! use crux_core::{
+//!     capability::{CapabilityContext, Operation},
+//!     Capability,
+//! };
+//! use serde::{Serialize, Deserialize};
+//!
+//! // A duck
+//! #[derive(Serialize, Deserialize, Clone, PartialEq, Eq, Debug)]
+//! struct Duck;
+//!
+//! // Operations that can be requested from the Shell
+//! #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq)]
+//! enum DuckOperation {
+//!     GetInARow(usize)
+//! }
+//!
+//! // Respective outputs for those operations
+//! #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq)]
+//! enum DuckOutput {
+//!     GetInRow(Vec<Duck>)
+//! }
+//!
+//! // Link the input and output type
+//! impl Operation for DuckOperation {
+//!     type Output = DuckOutput;
+//! }
+//!
+//! // The capability. Context will provide the interface to the rest of the system.
+//! struct Ducks<Event> {
+//!     context: CapabilityContext<DuckOperation, Event>
+//! };
+//!
+//! // Basic empty implementation of the capability
+//! impl<Event> Ducks<Event> {
+//!     pub fn new(context: CapabilityContext<DuckOperation, Event>) -> Self {
+//!         Self { context }
+//!     }
+//!
+//!     pub fn get_in_a_row<F>(&self, number_of_ducks: usize, event: F)
+//!     where
+//!         Event: 'static,
+//!         F: Fn(Vec<Duck>) -> Event + Send + 'static,
+//!     {
+//!         let ctx = self.context.clone();
+//!         // Start a shell interaction
+//!         self.context.spawn(async move {
+//!             // Instruct Shell to get ducks in a row and await the ducks
+//!             let ducks = ctx.request_from_shell(DuckOperation::GetInARow(number_of_ducks)).await;
+//!             
+//!             // Unwrap the ducks and wrap them in the requested event
+//!             // This will always succeed, as long as the Shell implementation is correct
+//!             // and doesn't send the wrong output type back
+//!             if let DuckOutput::GetInRow(ducks) = ducks {
+//!                 // Queue an app update with the ducks event
+//!                 ctx.update_app(event(ducks));
+//!             }
+//!         })
+//!    }
+//! }
+//! ```
+//!
+//! The `self.context.spawn` API allows a multi-step transaction with the Shell to be performed by a capability
+//! without involving the app, until the exchange has completed. During the exchange, one or more events can
+//! be emitted (allowing a subscription or streaming like capability to be built).
+//!
+//! For Shell requests that have no output, you can use [`CapabilityContext::notify_shell`].
+//!
+//! `DuckOperation` and `DuckOutput` show how the set of operations can be extended. In simple capabilities,
+//! with a single operation, these can be structs, or simpler types. For example, the HTTP capability works directly with
+//! `HttpRequest` and `HttpResponse`.
 
 use std::sync::Arc;
 
