e9dc64a7ca26f215e5124264129bd274418dfa78
Stuart Harris <stuart.harris@red-badger.com>
2022-12-26 19:36:04 +0000

    refactor impl and add test

--- crux_macros/src/effect.rs
+++ crux_macros/src/effect.rs
@@ -1,10 +1,10 @@
 use convert_case::{Case, Casing};
-use darling::{ast, util, FromDeriveInput, FromField, FromMeta};
+use darling::{ast, util, FromDeriveInput, FromField, FromMeta, ToTokens};
 use proc_macro2::TokenStream;
 use quote::quote;
 use syn::{DeriveInput, Ident, Type};
 
-#[derive(FromDeriveInput)]
+#[derive(FromDeriveInput, Debug)]
 #[darling(attributes(effect), supports(struct_named))]
 struct EffectStructReceiver {
     ident: Ident,
@@ -14,11 +14,90 @@ struct EffectStructReceiver {
     data: ast::Data<util::Ignored, EffectFieldReceiver>,
 }
 
-#[derive(Debug, FromField)]
+#[derive(FromField, Debug)]
 #[darling(attributes(effect))]
 pub struct EffectFieldReceiver {
     ident: Option<Ident>,
     operation: Option<Type>,
+}
+
+impl ToTokens for EffectStructReceiver {
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        let ident = &self.ident;
+
+        let name = match self.name {
+            Some(ref name) => quote!(#name),
+            None => {
+                let x = Type::from_string("Effect").unwrap();
+                quote!(#x)
+            }
+        };
+
+        let app = match self.app {
+            Some(ref app) => quote!(#app),
+            None => {
+                let x = Type::from_string("App").unwrap();
+                quote!(#x)
+            }
+        };
+
+        let event = match self.event {
+            Some(ref event) => quote!(#event),
+            None => {
+                let x = Type::from_string("Event").unwrap();
+                quote!(#x)
+            }
+        };
+
+        let fields = self
+            .data
+            .as_ref()
+            .take_struct()
+            .expect("Should never be enum")
+            .fields;
+
+        let (variants, fields): (Vec<_>, Vec<_>) = fields
+            .into_iter()
+            .map(|field| {
+                let (snake, pascal) = field
+                    .ident
+                    .as_ref()
+                    .map(|snake| {
+                        let pascal =
+                            Ident::new(&snake.to_string().to_case(Case::Pascal), snake.span());
+                        (quote!(#snake), quote!(#pascal))
+                    })
+                    .expect("We already told darling we're on a struct with named fields");
+
+                if let Some(operation) = &field.operation {
+                    (
+                        quote! {#pascal(#operation)},
+                        quote! {#snake: #pascal::new(context.with_effect(#name::#pascal))},
+                    )
+                } else {
+                    (
+                        quote! {#pascal},
+                        quote! {#snake: #pascal::new(context.with_effect(|_| #name::#pascal))},
+                    )
+                }
+            })
+            .unzip();
+
+        tokens.extend(quote! {
+            #[derive(Clone, Serialize, Deserialize, Debug, PartialEq, Eq)]
+            pub enum #name {
+                #(#variants),*
+            }
+
+            impl crux_core::WithContext<#app, #name> for #ident {
+                fn new_with_context(context: CapabilityContext<#name, #event>) -> #ident {
+                    #ident {
+                        #(#fields),*
+                    }
+                }
+            }
+        })
+    }
 }
 
 pub(crate) fn effect_impl(input: &DeriveInput) -> TokenStream {
@@ -29,64 +108,46 @@ pub(crate) fn effect_impl(input: &DeriveInput) -> TokenStream {
         }
     };
 
-    let ident = &input.ident;
-    let name = input
-        .name
-        .unwrap_or_else(|| Type::from_string("Effect").unwrap());
+    quote!(#input)
+}
 
-    let app = input
-        .app
-        .unwrap_or_else(|| Type::from_string("App").unwrap());
+#[cfg(test)]
+mod tests {
+    use darling::{FromDeriveInput, ToTokens};
+    use quote::quote;
+    use syn::parse_str;
 
-    let event = input
-        .event
-        .unwrap_or_else(|| Type::from_string("Event").unwrap());
+    use crate::effect::EffectStructReceiver;
 
-    let fields = input
-        .data
-        .as_ref()
-        .take_struct()
-        .expect("Should never be enum")
-        .fields;
-
-    let (variants, fields): (Vec<_>, Vec<_>) = fields
-        .into_iter()
-        .map(|field| {
-            let (snake, pascal) = field
-                .ident
-                .as_ref()
-                .map(|snake| {
-                    let pascal = Ident::new(&snake.to_string().to_case(Case::Pascal), snake.span());
-                    (quote!(#snake), quote!(#pascal))
-                })
-                .expect("We already told darling we're on a struct with named fields");
-
-            if let Some(operation) = &field.operation {
-                (
-                    quote! {#pascal(#operation)},
-                    quote! {#snake: #pascal::new(context.with_effect(#name::#pascal))},
-                )
-            } else {
-                (
-                    quote! {#pascal},
-                    quote! {#snake: #pascal::new(context.with_effect(|_| #name::#pascal))},
-                )
-            }
-        })
-        .unzip();
-
-    quote! {
-        #[derive(Clone, Serialize, Deserialize, Debug, PartialEq, Eq)]
-        pub enum #name {
-            #(#variants),*
+    #[test]
+    fn defaults() {
+        let input = r#"
+        #[derive(Effect)]
+        pub struct Capabilities {
+            pub render: Render<Event>,
         }
+        "#;
+        let parsed = parse_str(input).unwrap();
+        let input = EffectStructReceiver::from_derive_input(&parsed).unwrap();
 
-        impl crux_core::WithContext<#app, #name> for #ident {
-            fn new_with_context(context: CapabilityContext<#name, #event>) -> #ident {
-                #ident {
-                    #(#fields),*
+        let expected = quote! {
+            #[derive(Clone, Serialize, Deserialize, Debug, PartialEq, Eq)]
+            pub enum Effect {
+                Render
+            }
+
+            impl crux_core::WithContext<App, Effect> for Capabilities {
+                fn new_with_context(context: CapabilityContext<Effect, Event>) -> Capabilities {
+                    Capabilities {
+                        render: Render::new(context.with_effect(|_| Effect::Render))
+                    }
                 }
             }
-        }
+        };
+
+        let mut actual = quote!();
+        input.to_tokens(&mut actual);
+
+        assert_eq!(expected.to_string(), actual.to_string());
     }
 }
