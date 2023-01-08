c5fd4139ffd93e65896a4874150c47ee163bb4f6
Graeme Coupar <grambo@grambo.me.uk>
2023-01-08 14:59:02 +0000

    Remove crux_http::Config docstrings.
    There's not really a way to use this at present.  I do have an idea for
    how to enable it, but want to leave that for a future PR.

--- crux_http/src/config.rs
+++ crux_http/src/config.rs
@@ -7,24 +7,7 @@ use http_types::headers::{HeaderName, HeaderValues, ToHeaderValues};
 use crate::http::Url;
 use crate::Result;
 
-/// Configuration for `crux_http::Client`s and their underlying HTTP clients.
-///
-/// ```
-/// use std::convert::TryInto;
-/// use crux_http::{Client, Config, Url};
-///
-/// # #[async_std::main]
-/// # async fn main() -> crux_http::Result<()> {
-/// let client: Client = Config::new()
-///     .set_base_url(Url::parse("https://example.org")?)
-///     .try_into()?;
-///
-/// let mut response = client.get("/").await?;
-///
-/// println!("{}", response.body_string().await?);
-/// # Ok(())
-/// # }
-/// ```
+/// Configuration for `crux_http::Http`s and their underlying HTTP client.
 #[non_exhaustive]
 #[derive(Clone, Debug, Default)]
 pub struct Config {
@@ -46,24 +29,9 @@ impl Config {
 }
 
 impl Config {
-    /// Adds a header to be added to every request by this client.
+    /// Adds a header to be added to every request by this config.
     ///
     /// Default: No extra headers.
-    ///
-    /// ```
-    /// use std::convert::TryInto;
-    /// use crux_http::{Client, Config};
-    /// use crux_http::http::auth::BasicAuth;
-    ///
-    /// # fn main() -> crux_http::Result<()> {
-    /// let auth = BasicAuth::new("Username", "Password");
-    ///
-    /// let client: Client = Config::new()
-    ///     .add_header(auth.name(), auth.value())?
-    ///     .try_into()?;
-    /// # Ok(())
-    /// # }
-    /// ```
     pub fn add_header(
         mut self,
         name: impl Into<HeaderName>,
@@ -74,25 +42,13 @@ impl Config {
         Ok(self)
     }
 
-    /// Sets the base URL for this client. All request URLs will be relative to this URL.
+    /// Sets the base URL for this config. All request URLs will be relative to this URL.
     ///
     /// Note: a trailing slash is significant.
     /// Without it, the last path component is considered to be a “file” name
     /// to be removed to get at the “directory” that is used as the base.
     ///
     /// Default: `None` (internally).
-    ///
-    /// ```
-    /// use std::convert::TryInto;
-    /// use crux_http::{Client, Config, Url};
-    ///
-    /// # fn main() -> crux_http::Result<()> {
-    /// let client: Client = Config::new()
-    ///     .set_base_url(Url::parse("https://example.org")?)
-    ///     .try_into()?;
-    /// # Ok(())
-    /// # }
-    /// ```
     pub fn set_base_url(mut self, base: Url) -> Self {
         self.base_url = Some(base);
         self
