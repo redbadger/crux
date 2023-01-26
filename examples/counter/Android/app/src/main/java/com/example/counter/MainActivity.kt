a8efa1b230b72d4a2c2564d9ffaa303a626a48f7
Stuart Harris <stuart.harris@red-badger.com>
2023-01-26 11:39:15 +0000

    android

--- examples/counter/Android/app/src/main/java/com/example/counter/MainActivity.kt
+++ examples/counter/Android/app/src/main/java/com/example/counter/MainActivity.kt
@@ -23,13 +23,17 @@ import com.example.counter.shared.handleResponse
 import com.example.counter.shared.processEvent
 import com.example.counter.shared.view
 import com.example.counter.shared_types.Effect
-import com.example.counter.shared_types.Event
 import com.example.counter.shared_types.HttpResponse
 import com.example.counter.shared_types.Requests
+import com.example.counter.shared_types.SseResponse
+import com.example.counter.shared_types.SseResponse.Chunk
 import com.example.counter.ui.theme.CounterTheme
 import okhttp3.*
 import okhttp3.RequestBody.Companion.toRequestBody
+import java.io.BufferedReader
 import java.io.IOException
+import java.io.InputStreamReader
+import java.util.concurrent.TimeUnit
 import com.example.counter.shared_types.Event as Evt
 import com.example.counter.shared_types.Request as Req
 import com.example.counter.shared_types.ViewModel as MyViewModel
@@ -43,9 +47,7 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
-                ) {
-                    View()
-                }
+                ) { View() }
             }
         }
     }
@@ -53,6 +55,7 @@ class MainActivity : ComponentActivity() {
 
 sealed class Outcome {
     data class Http(val res: HttpResponse) : Outcome()
+    data class Sse(val res: SseResponse) : Outcome()
 }
 
 sealed class CoreMessage {
@@ -66,69 +69,105 @@ class Model : ViewModel() {
     private val client = OkHttpClient()
 
     init {
-        update(CoreMessage.Event(Event.Get()))
+        update(CoreMessage.Event(Evt.StartWatch()))
     }
 
     fun update(msg: CoreMessage) {
         val requests: List<Req> =
             when (msg) {
-                is CoreMessage.Event -> Requests.bcsDeserialize(
-                    processEvent(msg.event.bcsSerialize().toUByteArray().toList()).toUByteArray()
-                        .toByteArray()
-                )
-                is CoreMessage.Response -> Requests.bcsDeserialize(
-                    handleResponse(
-                        msg.uuid.toList(), when (msg.outcome) {
-                            is Outcome.Http -> msg.outcome.res.bcsSerialize()
-                        }.toUByteArray().toList()
-                    ).toUByteArray()
-                        .toByteArray()
-                )
+                is CoreMessage.Event ->
+                    Requests.bcsDeserialize(
+                        processEvent(msg.event.bcsSerialize().toUByteArray().toList())
+                            .toUByteArray()
+                            .toByteArray()
+                    )
+                is CoreMessage.Response ->
+                    Requests.bcsDeserialize(
+                        handleResponse(
+                            msg.uuid.toList(),
+                            when (msg.outcome) {
+                                is Outcome.Http -> msg.outcome.res.bcsSerialize()
+                                is Outcome.Sse -> msg.outcome.res.bcsSerialize()
+                            }.toUByteArray().toList()
+                        ).toUByteArray().toByteArray()
+                    )
             }
 
         for (req in requests) when (val effect = req.effect) {
             is Effect.Render -> {
                 this.view = MyViewModel.bcsDeserialize(view().toUByteArray().toByteArray())
             }
-            is Effect.Http -> {
-                httpRequest(effect.value.method, effect.value.url, req.uuid)
-            }
-            is Effect.ServerSentEvents -> {}
+            is Effect.Http -> http(req.uuid, effect.value.method, effect.value.url)
+            is Effect.ServerSentEvents -> sse(req.uuid, effect.value.url)
         }
     }
 
-    private fun httpRequest(method: String, url: String, uuid: List<Byte>) {
+    private fun http(uuid: List<Byte>, method: String, url: String) {
         var builder = Request.Builder().url(url)
         if (method == "POST") {
             builder = builder.method(method, byteArrayOf(0).toRequestBody(null, 0, 0))
-
         }
         val request = builder.build()
 
-        client.newCall(request).enqueue(object : Callback {
-            override fun onFailure(call: Call, e: IOException) {
-                e.printStackTrace()
-            }
-
-            override fun onResponse(call: Call, response: Response) {
-                response.use {
-                    if (!response.isSuccessful) throw IOException("Unexpected code $response")
-
-                    for ((name, value) in response.headers) {
-                        println("$name: $value")
+        client.newCall(request)
+            .enqueue(
+                object : Callback {
+                    override fun onFailure(call: Call, e: IOException) {
+                        e.printStackTrace()
                     }
 
-                    response.body!!.bytes().toList().let { bytes ->
-                        update(
-                            CoreMessage.Response(
-                                uuid.toByteArray().toUByteArray().toList(),
-                                Outcome.Http(HttpResponse(response.code.toShort(), bytes))
-                            )
-                        )
+                    override fun onResponse(call: Call, response: Response) {
+                        response.use {
+                            if (!response.isSuccessful)
+                                throw IOException("Unexpected code $response")
+
+                            response.body!!.bytes().toList().let { bytes ->
+                                update(
+                                    CoreMessage.Response(
+                                        uuid.toByteArray().toUByteArray().toList(),
+                                        Outcome.Http(HttpResponse(response.code.toShort(), bytes))
+                                    )
+                                )
+                            }
+                        }
                     }
                 }
-            }
-        })
+            )
+    }
+
+    private fun sse(uuid: List<Byte>, url: String) {
+        val sseClient: OkHttpClient = client.newBuilder()
+            .readTimeout(0, TimeUnit.SECONDS)
+            .build()
+
+        val request = Request.Builder().url(url).build()
+        sseClient.newCall(request)
+            .enqueue(
+                object : Callback {
+                    override fun onFailure(call: Call, e: IOException) {
+                        e.printStackTrace()
+                    }
+
+                    override fun onResponse(call: Call, response: Response) {
+                        response.use {
+                            if (!response.isSuccessful)
+                                throw IOException("Unexpected code $response")
+                            val reader =
+                                BufferedReader(InputStreamReader(response.body!!.byteStream()))
+                            var line: String
+                            while (reader.readLine().also { line = it } != null) {
+                                line += "\n\n"
+                                update(
+                                    CoreMessage.Response(
+                                        uuid.toByteArray().toUByteArray().toList(),
+                                        Outcome.Sse(Chunk(line.toByteArray().toList()))
+                                    )
+                                )
+                            }
+                        }
+                    }
+                }
+            )
     }
 }
 
@@ -146,14 +185,11 @@ fun View(model: Model = viewModel()) {
         Text(text = model.view.text, modifier = Modifier.padding(10.dp))
         Row(horizontalArrangement = Arrangement.spacedBy(10.dp)) {
             Button(
-                onClick = { model.update(CoreMessage.Event(Event.Decrement())) },
-                colors =
-                ButtonDefaults.buttonColors(
-                    containerColor = Color.hsl(44F, 1F, 0.77F)
-                )
+                onClick = { model.update(CoreMessage.Event(Evt.Decrement())) },
+                colors = ButtonDefaults.buttonColors(containerColor = Color.hsl(44F, 1F, 0.77F))
             ) { Text(text = "Decrement", color = Color.DarkGray) }
             Button(
-                onClick = { model.update(CoreMessage.Event(Event.Increment())) },
+                onClick = { model.update(CoreMessage.Event(Evt.Increment())) },
                 colors =
                 ButtonDefaults.buttonColors(
                     containerColor = Color.hsl(348F, 0.86F, 0.61F)
@@ -166,7 +202,5 @@ fun View(model: Model = viewModel()) {
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    CounterTheme {
-        View()
-    }
+    CounterTheme { View() }
 }
