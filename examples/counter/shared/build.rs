cdb641f81e441a8643f745c212e5581fca7961a8
Stuart Harris <stuart.harris@red-badger.com>
2023-03-23 17:15:12 +0000

    remove rustdoc-json crate

--- examples/counter/shared/build.rs
+++ examples/counter/shared/build.rs
@@ -1,12 +1,3 @@
 fn main() {
     uniffi::generate_scaffolding("./src/shared.udl").unwrap();
-
-    // let json_path = rustdoc_json::Builder::default()
-    //     .toolchain("nightly")
-    //     .manifest_path("./Cargo.toml")
-    //     .build()
-    //     .unwrap();
-
-    // // Prints `Wrote rustdoc JSON to "/Users/martin/src/project/target/doc/project.json"`
-    // println!("Wrote rustdoc JSON to {:?}", &json_path);
 }
