d137bcd7163ce97cb69b6fc6e8dc5cac241a6904
Stuart Harris <stuart.harris@red-badger.com>
2023-03-29 13:23:23 +0200

    hmm, some bits not implemented

--- crux_cli/src/main.rs
+++ crux_cli/src/main.rs
@@ -1,9 +1,8 @@
-use std::collections::BTreeMap;
-
 use clap::Parser;
 use clap_verbosity_flag::Verbosity;
 use crux_cli::{parse_crate, GlobalConfig};
-use trustfall::TransparentValue;
+use std::collections::BTreeMap;
+use trustfall::FieldValue;
 use trustfall_rustdoc::{VersionedIndexedCrate, VersionedRustdocAdapter};
 
 #[derive(Parser, Debug)]
@@ -26,13 +25,25 @@ fn main() -> anyhow::Result<()> {
 
     let adapter = VersionedRustdocAdapter::new(&indexed_crate, Some(&indexed_crate))
         .expect("failed to create adapter");
-    let args: BTreeMap<String, TransparentValue> = BTreeMap::new();
-    let results_iter = adapter.run_query(include_str!("./queries/implements_app.gql"), args)?;
+    let mut arguments = BTreeMap::new();
+    // let arguments: BTreeMap<String, FieldValue> = BTreeMap::new();
+    // arguments.insert("false", FieldValue::Boolean(false));
+    arguments.insert("public", FieldValue::String("public".to_string()));
+    // arguments.insert("app", FieldValue::String("App".to_string()));
+    // arguments.insert(
+    //     "app_trait",
+    //     FieldValue::List(vec![
+    //         FieldValue::String("crux_core".to_string()),
+    //         FieldValue::String("App".to_string()),
+    //     ]),
+    // );
+    let results_iter =
+        adapter.run_query(include_str!("./queries/implements_app.gql"), arguments)?;
     let actual_results: Vec<BTreeMap<_, _>> = results_iter
         .map(|res| res.into_iter().map(|(k, v)| (k.to_string(), v)).collect())
         .collect();
 
-    config.shell_status("found", format!("types: {actual_results:#?}"))?;
+    config.shell_status("Found", format!("types: {actual_results:#?}"))?;
 
     Ok(())
 }
