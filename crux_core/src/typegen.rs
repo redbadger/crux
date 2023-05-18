ec7b4e4a59222775cc8fdd1e73ec81c974586c65
Stuart Harris <stuart.harris@red-badger.com>
2023-05-18 13:29:02 +0100

    v0.5.0

--- crux_core/src/typegen.rs
+++ crux_core/src/typegen.rs
@@ -10,7 +10,7 @@
 //!
 //! ```rust,ignore
 //! [build-dependencies]
-//! crux_core = { version = "0.4", features = ["typegen"] }
+//! crux_core = { version = "0.5", features = ["typegen"] }
 //! ```
 //!
 //! * Your `shared_types` library, will have an empty `lib.rs`, since we only use it for generating foreign language type declarations.
