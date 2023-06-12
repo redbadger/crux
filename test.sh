75e20421e6ffed1161d3dfdda03f151350a24540
Stuart Harris <stuart.harris@red-badger.com>
2023-06-12 10:00:20 +0100

    run integration tests in CI

--- test.sh
+++ test.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# run the unit tests
 cargo nextest run --all-features
 
+# run the integration tests
+cargo test --test '*'
+
+# run the doc tests
 cargo test --doc --all-features
 
 for example in ./examples/*; do
