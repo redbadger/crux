8b197b688ab95318db902c87591b4df3cf1106d0
Stuart Harris <stuart.harris@red-badger.com>
2023-07-01 19:11:11 +0100

    more swift tidying

--- examples/counter/iOS/CounterApp/ContentView.swift
+++ examples/counter/iOS/CounterApp/ContentView.swift
@@ -20,40 +20,38 @@ class Model: ObservableObject {
     init() {
         update(msg: .event(.startWatch))
     }
-    
+
     func update(msg: Message) {
-        var reqs: [Request]
+        var requests: [Request]
 
         switch msg {
-        case let .event(m):
-            reqs = try! [Request].bincodeDeserialize(
-                input: [UInt8](processEvent(Data(try! m.bincodeSerialize())))
+        case let .event(event):
+            requests = try! .bincodeDeserialize(
+                input: [UInt8](processEvent(Data(try! event.bincodeSerialize())))
             )
-        case let .response(uuid, .http(r)):
-            reqs = try! [Request].bincodeDeserialize(
-                input: [UInt8](handleResponse(Data(uuid), Data(try! r.bincodeSerialize())))
+        case let .response(uuid, .http(httpRes)):
+            requests = try! .bincodeDeserialize(
+                input: [UInt8](handleResponse(Data(uuid), Data(try! httpRes.bincodeSerialize())))
             )
-        case let .response(uuid, .sse(r)):
-            reqs = try! [Request].bincodeDeserialize(
-                input: [UInt8](handleResponse(Data(uuid), Data(try! r.bincodeSerialize())))
+        case let .response(uuid, .sse(sseRes)):
+            requests = try! .bincodeDeserialize(
+                input: [UInt8](handleResponse(Data(uuid), Data(try! sseRes.bincodeSerialize())))
             )
         }
 
-        for req in reqs {
-            switch req.effect {
+        for request in requests {
+            switch request.effect {
             case .render:
-                view = try! ViewModel.bincodeDeserialize(
-                    input: [UInt8](CounterApp.view())
-                )
+                view = try! .bincodeDeserialize(input: [UInt8](CounterApp.view()))
             case let .http(httpReq):
                 Task {
-                    let res = try! await http(request: httpReq).get()
-                    update(msg: .response(req.uuid, .http(res)))
+                    let res = try! await httpRequest(httpReq).get()
+                    update(msg: .response(request.uuid, .http(res)))
                 }
             case let .serverSentEvents(sseReq):
                 Task {
-                    for await result in await sse(request: sseReq) {
-                        update(msg: .response(req.uuid, .sse(try! result.get())))
+                    for await result in await sseRequest(sseReq) {
+                        update(msg: .response(request.uuid, .sse(try! result.get())))
                     }
                 }
             }
@@ -111,64 +109,5 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView(model: Model())
-    }
-}
-
-enum HttpError : Error {
-    case generic(Error)
-    case message(String)
-}
-
-func http(request: HttpRequest) async -> Result<HttpResponse, HttpError> {
-    var req = URLRequest(url: URL(string: request.url)!)
-    req.httpMethod = request.method
-    
-    for header in request.headers {
-        req.addValue(header.value, forHTTPHeaderField: header.name)
-    }
-    
-    do {
-        let (data, response) = try await URLSession.shared.data(for: req)
-        if let httpResponse = response as? HTTPURLResponse {
-            let status = UInt16(httpResponse.statusCode)
-            let body = [UInt8](data)
-            return .success(HttpResponse(status: status, body: body))
-        } else {
-            return .failure(.message("bad response"))
-        }
-    }
-    catch {
-        return .failure(.generic(error))
-    }
-    
-}
-
-func sse(request: SseRequest) async -> AsyncStream<Result<SseResponse, HttpError>> {
-    return AsyncStream { continuation in
-        Task {
-            let req = URLRequest(url: URL(string: request.url)!)
-            do {
-                let (asyncBytes, response) = try await URLSession.shared.bytes(for: req)
-                if let httpResponse = response as? HTTPURLResponse {
-                    if !(200 ... 299).contains(httpResponse.statusCode) {
-                        continuation.yield(.failure(
-                            .message("error, status code: \(httpResponse.statusCode)")
-                        ))
-                        continuation.finish()
-                        return
-                    }
-                }
-                
-                for try await line in asyncBytes.lines {
-                    let line = line + "\n\n"
-                    continuation.yield(.success(.chunk([UInt8](line.utf8))))
-                }
-                continuation.yield(.success(.done))
-                continuation.finish()
-            } catch {
-                continuation.yield(.failure(.generic(error)))
-                continuation.finish()
-            }
-        }
     }
 }
