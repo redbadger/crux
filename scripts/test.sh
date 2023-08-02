0614dea838cdb8c94a2524f7c7b1b41e95721858
Stuart Harris <stuart.harris@red-badger.com>
2023-08-02 15:07:32 +0100

    collapse build to single job to save time/carbon

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-set -e
+set -euxo pipefail
 
 for dir in . ./examples/*; do
   (
     cd "$dir"
     cargo nextest run --all-features
     cargo test --doc --all-features
+    cargo fmt --all --check
   )
 done
