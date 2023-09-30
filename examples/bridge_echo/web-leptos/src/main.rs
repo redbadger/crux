2a1c0a081f362d64dbf046806388c7ff6de0a80f
Stuart Harris <stuart.harris@red-badger.com>
2023-09-30 20:47:19 +0100

    update leptos examples to remove Scope

--- examples/bridge_echo/web-leptos/src/main.rs
+++ examples/bridge_echo/web-leptos/src/main.rs
@@ -3,31 +3,31 @@ mod core;
 use std::time::Duration;
 
 use leptos::{
-    component, create_effect, create_signal, set_timeout, view, IntoView, Scope, SignalGet,
-    SignalSet, SignalUpdate,
+    component, create_effect, create_signal, set_timeout, view, IntoView, SignalGet, SignalSet,
+    SignalUpdate,
 };
 
 use shared::Event;
 
 #[component]
-fn RootComponent(cx: Scope) -> impl IntoView {
+fn RootComponent() -> impl IntoView {
     let core = core::new();
-    let (view, render) = create_signal(cx, core.view());
-    let (event, set_event) = create_signal(cx, Event::Tick);
-    let (clock, set_clock) = create_signal(cx, 0u64);
+    let (view, render) = create_signal(core.view());
+    let (event, set_event) = create_signal(Event::Tick);
+    let (clock, set_clock) = create_signal(0u64);
 
-    create_effect(cx, move |_| {
+    create_effect(move |_| {
         core::update(&core, event.get(), render);
     });
 
     // When render happens, send a new tick immediately
-    create_effect(cx, move |_| {
+    create_effect(move |_| {
         view.get();
         set_timeout(move || set_event.set(Event::Tick), Duration::from_millis(0));
     });
 
     // Start a new period every second
-    create_effect(cx, move |_| {
+    create_effect(move |_| {
         clock.get();
         set_timeout(
             move || {
@@ -38,7 +38,7 @@ fn RootComponent(cx: Scope) -> impl IntoView {
         )
     });
 
-    view! {cx,
+    view! {
         <section class="box container has-text-centered m-5">
             <p class="is-size-5">{move || view.get().count}</p>
         </section>
@@ -49,7 +49,7 @@ fn main() {
     _ = console_log::init_with_level(log::Level::Debug);
     console_error_panic_hook::set_once();
 
-    leptos::mount_to_body(|cx| {
-        view! { cx, <RootComponent /> }
+    leptos::mount_to_body(|| {
+        view! { <RootComponent /> }
     });
 }
