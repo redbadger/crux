c95568095057a3930ec44ee8a86f5026d27f5a13
Stuart Harris <stuart.harris@red-badger.com>
2023-11-28 16:08:44 +0000

    root deps

--- scripts/update_deps.sh
+++ scripts/update_deps.sh
@@ -2,10 +2,14 @@
 
 set -e
 
+# until https://github.com/killercup/cargo-edit/pull/870 is merged
+CARGO_REGISTRIES_CRATES_IO_PROTOCOL=git cargo fetch
+
 for dir in . ./examples/*; do
   (
     cd "$dir"
-    cargo upgrade -i --verbose
+    # until https://github.com/killercup/cargo-edit/pull/870 is merged
+    CARGO_REGISTRIES_CRATES_IO_PROTOCOL=git cargo upgrade -i --verbose
     cargo update
   )
 done
