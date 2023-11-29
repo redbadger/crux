7901e74a1b945f56305f105aec5b0ebb832d0e55
Stuart Harris <stuart.harris@red-badger.com>
2023-11-29 17:30:19 +0000

    deps and new update_deps script

--- scripts/update_deps.sh
+++ scripts/update_deps.sh
@@ -1,12 +1,27 @@
-#!/bin/bash
+#!/opt/homebrew/bin/bash
 
 set -e
 
 # until https://github.com/killercup/cargo-edit/pull/870 is merged
 CARGO_REGISTRIES_CRATES_IO_PROTOCOL=git cargo fetch
 
-for dir in . ./examples/*; do
+mapfile -t all < <(fd Cargo.toml)
+roots=()
+for file in "${all[@]}"; do
+  if [[ $file == *"templates"* ]]; then
+    continue
+  fi
+  roots+=("$(
+    cd "$(dirname "$file")"
+    cargo metadata --format-version 1 | jq -r '.workspace_root'
+  )")
+done
+
+readarray -t sorted < <(printf '%s\n' "${roots[@]}" | sort -u)
+
+for dir in "${sorted[@]}"; do
   (
+    echo "---  Updating dependencies in $dir"
     cd "$dir"
     # until https://github.com/killercup/cargo-edit/pull/870 is merged
     CARGO_REGISTRIES_CRATES_IO_PROTOCOL=git cargo upgrade -i --verbose
