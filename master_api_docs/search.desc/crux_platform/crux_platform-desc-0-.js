791478bf4c7ef17806974bfd89d40c43f96cb73b
StuartHarris <StuartHarris@users.noreply.github.com>
2024-07-26 09:21:04 +0000

    deploy: ed80cddbe9504c38a8a125f33ff393c8b01ba626

---
+++ master_api_docs/search.desc/crux_platform/crux_platform-desc-0-.js
@@ -0,0 +1,1 @@
+searchState.loadedDescShard("crux_platform", 0, "TODO mod docs\nReturns the argument unchanged.\nReturns the argument unchanged.\nReturns the argument unchanged.\nCalls <code>U::from(self)</code>.\nCalls <code>U::from(self)</code>.\nCalls <code>U::from(self)</code>.")
---
+++ master_api_docs/trait.impl/clap_builder/derive/trait.CommandFactory.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"crux":[["impl CommandFactory for <a class=\"struct\" href=\"crux/args/struct.Cli.html\" title=\"struct crux::args::Cli\">Cli</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
