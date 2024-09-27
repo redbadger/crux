875dd59f04d0adccb02aca047db2e477c31ee97f
Stuart Harris <stuart.harris@red-badger.com>
2024-09-27 15:12:27 +0100

    all examples in script, and lockfiles

--- crux_cli/codegen.fish
+++ crux_cli/codegen.fish
@@ -1,6 +1,12 @@
 #!/usr/bin/env fish
 
 cargo build
-pushd ../examples/counter
-../../target/debug/crux codegen --lib shared
-popd
+
+for d in ../examples/*
+    echo ""
+    echo "---------------"
+    echo "Public API for $d"
+    pushd $d
+    ../../target/debug/crux codegen --lib shared
+    popd
+end
