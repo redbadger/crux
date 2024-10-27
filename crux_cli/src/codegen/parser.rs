76aba96570b1bae67adde631a34291f4ad5b7473
Stuart Harris <stuart.harris@red-badger.com>
2024-10-27 14:17:06 +0000

    ascent wip

--- crux_cli/src/codegen/parser.rs
+++ crux_cli/src/codegen/parser.rs
@@ -1,27 +1,72 @@
 use std::collections::HashMap;
 
 use anyhow::{anyhow, Result};
-use petgraph::{dot::Dot, graph::NodeIndex, Graph};
-use rustdoc_types::{Crate, Enum, Id, Impl, Item, ItemEnum, Type, VariantKind};
+use ascent::ascent;
+use rustdoc_types::{
+    Crate, Enum, Id, Impl, Item, ItemEnum, ItemSummary, Path, StructKind, Type, VariantKind,
+};
+use serde::Serialize;
+
+ascent! {
+    relation edge(Node, Node, Edge);
+
+    relation implements(Node, Node);
+    relation struct_fields(Node, Node);
+    relation enum_variants(Node, Node);
+    relation associated_type(Node, Node);
+
+    implements(type_, trait_) <--
+        edge(impl_, type_, Edge::ForType),
+        edge(impl_, trait_, Edge::Trait);
+
+    associated_type(impl_, type_) <--
+        edge(impl_, trait_, Edge::Trait),
+        edge(impl_, item, Edge::AssociatedItem),
+        edge(item, type_, Edge::AssociatedType);
+
+    struct_fields(struct_, field) <--
+        associated_type(impl_, struct_),
+        (edge(struct_, field, Edge::HasField) || edge(struct_, field, Edge::Unit));
+
+    enum_variants(enum_, variant) <--
+        associated_type(impl_, enum_),
+        edge(enum_, variant, Edge::HasVariant);
+
+    enum_variants(variant, field) <--
+        enum_variants(enum_, variant),
+        edge(variant, field, Edge::HasField);
+}
 
 pub fn parse(crate_: &Crate) -> Result<String> {
-    let mut graph = Graph::new();
-    let mut nodes = HashMap::new();
+    let mut prog = AscentProgram::default();
+    let mut nodes_by_id = HashMap::new();
 
-    // nodes
-    for (id, item) in crate_.index.clone() {
-        nodes.insert(id, graph.add_node(Node { id, item }));
+    // items
+    for (id, item) in &crate_.index {
+        nodes_by_id
+            .entry(*id)
+            .or_insert_with(|| Node::new(*id))
+            .item = Some(item.clone());
     }
 
-    let node = |id| -> Result<&NodeIndex> {
-        nodes
+    // paths
+    for (id, path) in &crate_.paths {
+        nodes_by_id
+            .entry(*id)
+            .or_insert_with(|| Node::new(*id))
+            .path = Some(path.clone());
+    }
+
+    let node_by_id = |id: &Id| -> Result<&Node> {
+        nodes_by_id
             .get(id)
             .ok_or_else(|| anyhow!("Could not find node with id {:?}", id))
     };
 
     // edges
     for (id, item) in &crate_.index {
-        let source = node(id)?;
+        let source = node_by_id(id)?.clone();
+
         match &item.inner {
             ItemEnum::Module(_module) => (),
             ItemEnum::ExternCrate { name: _, rename: _ } => (),
@@ -29,43 +74,65 @@ pub fn parse(crate_: &Crate) -> Result<String> {
             ItemEnum::Union(_union) => (),
             ItemEnum::Struct(s) => {
                 match &s.kind {
-                    rustdoc_types::StructKind::Unit => (),
-                    rustdoc_types::StructKind::Tuple(fields) => {
+                    StructKind::Unit => {
+                        prog.edge.push((source.clone(), source, Edge::Unit));
+                    }
+                    StructKind::Tuple(fields) => {
                         for field in fields {
                             if let Some(id) = field {
-                                graph.add_edge(*source, *node(&id)?, Edge::HasField);
+                                prog.edge.push((
+                                    source.clone(),
+                                    node_by_id(&id)?.clone(),
+                                    Edge::HasField,
+                                ));
                             }
                         }
                     }
-                    rustdoc_types::StructKind::Plain {
+                    StructKind::Plain {
                         fields,
                         has_stripped_fields: _,
                     } => {
                         for id in fields {
-                            graph.add_edge(*source, *node(&id)?, Edge::HasField);
+                            prog.edge.push((
+                                source.clone(),
+                                node_by_id(&id)?.clone(),
+                                Edge::HasField,
+                            ));
                         }
                     }
                 };
-                for id in &s.impls {
-                    graph.add_edge(*source, *node(&id)?, Edge::Implements);
-                }
             }
             ItemEnum::StructField(_) => (),
             ItemEnum::Enum(Enum { variants, .. }) => {
                 for id in variants {
-                    graph.add_edge(*source, *node(&id)?, Edge::HasVariant);
+                    prog.edge
+                        .push((source.clone(), node_by_id(&id)?.clone(), Edge::HasVariant));
                 }
             }
             ItemEnum::Variant(v) => {
                 match &v.kind {
                     VariantKind::Plain => (),
-                    VariantKind::Tuple(_vec) => (),
+                    VariantKind::Tuple(fields) => {
+                        for id in fields {
+                            if let Some(id) = id {
+                                prog.edge.push((
+                                    source.clone(),
+                                    node_by_id(id)?.clone(),
+                                    Edge::HasField,
+                                ));
+                            }
+                        }
+                    }
                     VariantKind::Struct {
                         fields,
                         has_stripped_fields: _,
                     } => {
                         for field in fields {
-                            graph.add_edge(*source, *node(&field)?, Edge::HasField);
+                            prog.edge.push((
+                                source.clone(),
+                                node_by_id(field)?.clone(),
+                                Edge::HasField,
+                            ));
                         }
                     }
                 };
@@ -74,13 +141,32 @@ pub fn parse(crate_: &Crate) -> Result<String> {
             ItemEnum::Trait(_) => (),
             ItemEnum::TraitAlias(_trait_alias) => (),
             ItemEnum::Impl(Impl {
+                trait_:
+                    Some(Path {
+                        id: trait_id,
+                        name,
+                        args: _,
+                    }),
                 for_: Type::ResolvedPath(target),
                 items,
                 ..
             }) => {
-                graph.add_edge(*source, *node(&target.id)?, Edge::ImplFor);
+                if !["App", "Effect"].contains(&name.as_str()) {
+                    continue;
+                }
+                prog.edge.push((
+                    source.clone(),
+                    node_by_id(&target.id)?.clone(),
+                    Edge::ForType,
+                ));
+                prog.edge
+                    .push((source.clone(), node_by_id(trait_id)?.clone(), Edge::Trait));
                 for id in items {
-                    graph.add_edge(*source, *node(&id)?, Edge::AssociatedItem);
+                    prog.edge.push((
+                        source.clone(),
+                        node_by_id(&id)?.clone(),
+                        Edge::AssociatedItem,
+                    ));
                 }
             }
             ItemEnum::Impl(_) => (),
@@ -100,29 +186,59 @@ pub fn parse(crate_: &Crate) -> Result<String> {
                 bounds: _,
                 type_: Some(Type::ResolvedPath(target)),
             } => {
-                if let Ok(dest) = node(&target.id) {
-                    graph.add_edge(*source, *dest, Edge::AssociatedType);
+                if let Ok(dest) = node_by_id(&target.id) {
+                    prog.edge
+                        .push((source.clone(), dest.clone(), Edge::AssociatedType));
                 }
             }
             ItemEnum::AssocType { .. } => (),
         }
     }
-    let out = Dot::new(&graph);
-    Ok(format!("{:?}", out))
+
+    prog.run();
+
+    std::fs::write(
+        "/tmp/struct_fields.json",
+        serde_json::to_string(&prog.struct_fields).unwrap(),
+    )?;
+    std::fs::write(
+        "/tmp/enum_variants.json",
+        serde_json::to_string(&prog.enum_variants).unwrap(),
+    )?;
+
+    Ok(format!(""))
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, PartialEq, Eq, Serialize)]
 struct Node {
     id: Id,
-    item: Item,
+    item: Option<Item>,
+    path: Option<ItemSummary>,
 }
 
-#[derive(Debug)]
+impl Node {
+    fn new(id: Id) -> Self {
+        Self {
+            id,
+            item: None,
+            path: None,
+        }
+    }
+}
+
+impl std::hash::Hash for Node {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.id.hash(state);
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize)]
 enum Edge {
-    ImplFor,
-    HasField,
-    Implements,
     AssociatedItem,
     AssociatedType,
+    ForType,
+    HasField,
     HasVariant,
+    Trait,
+    Unit,
 }
