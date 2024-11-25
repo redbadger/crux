abc8cb464af9683b189f2d52d3e19fa25999dbba
Stuart Harris <stuart.harris@red-badger.com>
2024-11-25 08:54:08 +0000

    ascent driven IR gen WIP

--- crux_cli/src/codegen/parser.rs
+++ crux_cli/src/codegen/parser.rs
@@ -1,40 +1,47 @@
+use std::collections::BTreeMap;
+
 use anyhow::Result;
 use ascent::ascent;
 use rustdoc_types::{
-    Enum, GenericArg, GenericArgs, Impl, Item, ItemEnum, Path, StructKind, Type, VariantKind,
+    Enum, GenericArg, GenericArgs, Impl, Item, ItemEnum, Path, Struct, StructKind, Type, Variant,
+    VariantKind,
 };
 use serde::Serialize;
 
-use super::data::{Data, Node};
+use crate::codegen::format::ContainerFormat;
+
+use super::{
+    data::{Data, Node},
+    format::{Format, Named, VariantFormat},
+};
 
 ascent! {
-    // input data
+    // facts
+
     relation edge(Node, Node, Edge);
 
-    // result data
-    relation app(Node);
-    relation effect(Node);
-    relation is_effect_of_app(Node, Node);
-    relation root(Node);
-    relation parent(Node, Node);
-    relation output(Node, Node);
+    // rules
 
+    relation app(Node);
     // app structs have an implementation of the App trait
     app(app) <--
         edge(app_impl, app_trait, Edge::TraitApp),
         edge(app_impl, app, Edge::Type);
 
+    relation effect(Node);
     // effect enums have an implementation of the Effect trait
     effect(effect) <--
         edge(effect_impl, effect_trait, Edge::TraitEffect),
         edge(effect_impl, effect, Edge::Type);
 
+    relation is_effect_of_app(Node, Node);
     // an effect belongs to an app if they are in the same module
     is_effect_of_app(app, effect) <--
         app(app),
         effect(effect),
         if are_in_same_module(app, effect);
 
+    relation root(Node);
     // Event and ViewModel types are associated
     // with the root apps (that have no parent)
     root(assoc_type) <--
@@ -48,6 +55,7 @@ ascent! {
         is_effect_of_app(app, effect_enum),
         !parent(_, app);
 
+    relation parent(Node, Node);
     // app hierarchy
     parent(parent, child) <--
         app(parent),
@@ -55,12 +63,245 @@ ascent! {
         edge(parent, field, Edge::Field),
         edge(field, child, Edge::Type);
 
-    output(root, child) <--
+    relation subset(Node, Node);
+    subset(root, child) <--
         root(root),
         edge(root, child, ?Edge::Variant|Edge::Field);
-    output(parent, child) <--
-        output(grandparent, parent),
+    subset(parent, child) <--
+        subset(grandparent, parent),
         edge(parent, child, ?Edge::Variant|Edge::Field|Edge::Type);
+
+    relation is_enum(Node);
+    is_enum(enum_) <--
+        subset(enum_, _),
+        if let Some(Item { inner: ItemEnum::Enum(_), .. }) = &enum_.item;
+
+    relation is_struct(Node);
+    is_struct(struct_) <--
+        subset(struct_, _),
+        if let Some(Item { inner: ItemEnum::Struct(_), .. }) = &struct_.item;
+
+    relation struct_plain(Node);
+    struct_plain(struct_) <--
+        is_struct(struct_),
+        if let Some(Item { inner: ItemEnum::Struct(Struct{kind: StructKind::Plain { .. }, ..}), .. }) = &struct_.item;
+
+    relation struct_tuple(Node);
+    struct_tuple(struct_) <--
+        is_struct(struct_),
+        if let Some(Item { inner: ItemEnum::Struct(Struct{kind: StructKind::Tuple(_), ..}), .. }) = &struct_.item;
+
+    relation struct_field(Node, Node);
+    struct_field(struct_, field) <--
+        is_struct(struct_),
+        edge(struct_, field, Edge::Field);
+
+    relation enum_variant(Node, Node);
+    enum_variant(enum_, variant) <--
+        is_enum(enum_),
+        edge(enum_, variant, Edge::Variant);
+
+    relation variant_plain(Node, Node);
+    variant_plain(enum_, variant) <--
+        enum_variant(enum_, variant),
+        if let Some(Item { inner: ItemEnum::Variant(Variant{kind: VariantKind::Plain, ..}), .. }) = &variant.item;
+
+    relation variant_struct(Node, Node);
+    variant_struct(enum_, variant) <--
+        enum_variant(enum_, variant),
+        if let Some(Item { inner: ItemEnum::Variant(Variant{kind: VariantKind::Struct {..}, ..}), .. }) = &variant.item;
+
+    relation variant_tuple(Node, Node);
+    variant_tuple(enum_, variant) <--
+        enum_variant(enum_, variant),
+        if let Some(Item { inner: ItemEnum::Variant(Variant{kind: VariantKind::Tuple(_), ..}), .. }) = &variant.item;
+
+    relation variant_field(Node, Node);
+    variant_field(variant, field) <--
+        enum_variant(enum_, variant),
+        edge(variant, field, Edge::Field);
+
+    relation format(Node, Format);
+    format(struct_, format) <--
+        struct_field(struct_, field),
+        let format = make_format(&field);
+
+    relation format_named(Node, Named<Format>);
+    format_named(struct_, format) <--
+        struct_field(struct_, field),
+        let format = make_named_format(&field);
+
+    relation format_plain_variant(Node, Named<VariantFormat>);
+    format_plain_variant(enum_, format) <--
+        variant_plain(enum_, variant),
+        variant_field(variant, field),
+        agg formats = collect(format) in format_named(field, format),
+        let format = make_plain_variant_format(variant);
+
+    relation format_struct_variant(Node, Named<VariantFormat>);
+    format_struct_variant(enum_, format) <--
+        variant_struct(enum_, variant),
+        variant_field(variant, field),
+        agg formats = collect(format) in format_named(field, format),
+        let format = make_struct_variant_format(variant, &formats);
+
+    relation format_tuple_variant(Node, Named<VariantFormat>);
+    format_tuple_variant(enum_, format) <--
+        variant_tuple(enum_, variant),
+        variant_field(variant, field),
+        agg formats = collect(format) in format(field, format),
+        let format = make_tuple_variant_format(variant, &formats);
+
+    relation format_variant(Node, Named<VariantFormat>);
+    format_variant(enum_, format) <--
+        (
+            format_plain_variant(enum_, format) ||
+            format_struct_variant(enum_, format) ||
+            format_tuple_variant(enum_, format)
+        );
+
+    relation container(String, ContainerFormat);
+    container(name, container) <--
+        struct_plain(struct_),
+        agg formats = collect(format) in format_named(struct_, format),
+        if let Some(Item { name: Some(n), .. }) = &struct_.item,
+        let name = n.to_string(),
+        let container = make_struct_plain(struct_, &formats);
+    container(name, container) <--
+        struct_tuple(struct_),
+        agg formats = collect(format) in format(struct_, format),
+        if let Some(Item { name: Some(n), .. }) = &struct_.item,
+        let name = n.to_string(),
+        let container = make_struct_tuple(&formats);
+    container(name, container) <--
+        is_enum(enum_),
+        agg formats = collect(format) in format_variant(enum_, format),
+        if let Some(Item { name: Some(n), .. }) = &enum_.item,
+        let name = n.to_string(),
+        let container = make_enum(&formats);
+}
+
+pub fn collect<'a, N: 'a, T: Iterator<Item = (&'a N,)>>(
+    input: T,
+) -> impl Iterator<Item = Vec<(&'a N,)>>
+where
+    N: Clone,
+{
+    std::iter::once(input.collect::<Vec<_>>())
+}
+
+fn make_format(field: &Node) -> Format {
+    if let Some(item) = &field.item {
+        if let ItemEnum::StructField(type_) = &item.inner {
+            return type_.into();
+        };
+    }
+    unreachable!()
+}
+
+fn make_named_format(field: &Node) -> Named<Format> {
+    if let Some(Item {
+        name: Some(name), ..
+    }) = &field.item
+    {
+        return Named {
+            name: name.clone(),
+            value: make_format(field),
+        };
+    };
+    unreachable!()
+}
+
+fn make_plain_variant_format(variant: &Node) -> Named<VariantFormat> {
+    if let Some(Item {
+        name: Some(name), ..
+    }) = &variant.item
+    {
+        if let Some(item) = &variant.item {
+            if let ItemEnum::Variant(_) = &item.inner {
+                return Named {
+                    name: name.clone(),
+                    value: VariantFormat::Unit,
+                };
+            };
+        }
+    }
+    unreachable!()
+}
+
+fn make_struct_variant_format(
+    variant: &Node,
+    fields: &Vec<(&Named<Format>,)>,
+) -> Named<VariantFormat> {
+    if let Some(Item {
+        name: Some(name), ..
+    }) = &variant.item
+    {
+        if let Some(item) = &variant.item {
+            if let ItemEnum::Variant(_) = &item.inner {
+                return Named {
+                    name: name.clone(),
+                    value: VariantFormat::Struct(
+                        fields
+                            .iter()
+                            .map(|(field,)| (*field).clone())
+                            .collect::<Vec<_>>(),
+                    ),
+                };
+            };
+        }
+    }
+    unreachable!()
+}
+
+fn make_tuple_variant_format(variant: &Node, fields: &Vec<(&Format,)>) -> Named<VariantFormat> {
+    if let Some(Item {
+        name: Some(name), ..
+    }) = &variant.item
+    {
+        if let Some(item) = &variant.item {
+            if let ItemEnum::Variant(_) = &item.inner {
+                return Named {
+                    name: name.clone(),
+                    value: VariantFormat::Tuple(
+                        fields
+                            .iter()
+                            .map(|(field,)| (*field).clone())
+                            .collect::<Vec<_>>(),
+                    ),
+                };
+            };
+        }
+    }
+    unreachable!()
+}
+
+fn make_struct_plain(_node: &Node, fields: &Vec<(&Named<Format>,)>) -> ContainerFormat {
+    ContainerFormat::Struct(
+        fields
+            .iter()
+            .map(|(field,)| (*field).clone())
+            .collect::<Vec<_>>(),
+    )
+}
+
+fn make_struct_tuple(fields: &Vec<(&Format,)>) -> ContainerFormat {
+    ContainerFormat::TupleStruct(
+        fields
+            .iter()
+            .map(|(field,)| (*field).clone())
+            .collect::<Vec<_>>(),
+    )
+}
+
+fn make_enum(variants: &Vec<(&Named<VariantFormat>,)>) -> ContainerFormat {
+    ContainerFormat::Enum(
+        variants
+            .iter()
+            .enumerate()
+            .map(|(i, (variant,))| (i as u32, (*variant).clone()))
+            .collect::<BTreeMap<_, _>>(),
+    )
 }
 
 fn are_in_same_module(app: &Node, effect: &Node) -> bool {
@@ -73,7 +314,7 @@ fn are_in_same_module(app: &Node, effect: &Node) -> bool {
     app.path[..(app.path.len() - 1)] == effect.path[..(effect.path.len() - 1)]
 }
 
-pub fn parse(data: &Data) -> Result<Vec<(Node, Node)>> {
+pub fn parse(data: &Data) -> Result<Vec<(String, ContainerFormat)>> {
     let mut prog = AscentProgram::default();
 
     // edges
@@ -92,7 +333,10 @@ pub fn parse(data: &Data) -> Result<Vec<(Node, Node)>> {
             ItemEnum::Union(_union) => (),
             ItemEnum::Struct(s) => {
                 match &s.kind {
-                    StructKind::Unit => (),
+                    StructKind::Unit => {
+                        prog.edge
+                            .push((source.clone(), source.clone(), Edge::Field))
+                    }
                     StructKind::Tuple(fields) => {
                         for field in fields {
                             if let Some(id) = field {
@@ -140,7 +384,10 @@ pub fn parse(data: &Data) -> Result<Vec<(Node, Node)>> {
             }
             ItemEnum::Variant(v) => {
                 match &v.kind {
-                    VariantKind::Plain => (),
+                    VariantKind::Plain => {
+                        prog.edge
+                            .push((source.clone(), source.clone(), Edge::Field));
+                    }
                     VariantKind::Tuple(fields) => {
                         for id in fields {
                             if let Some(id) = id {
@@ -258,28 +505,7 @@ pub fn parse(data: &Data) -> Result<Vec<(Node, Node)>> {
 
     prog.run();
 
-    // write field and variant edges to disk for debugging
-    std::fs::write("/tmp/edge.json", serde_json::to_string(&prog.edge).unwrap())?;
-    std::fs::write("/tmp/app.json", serde_json::to_string(&prog.app).unwrap())?;
-    std::fs::write(
-        "/tmp/effect.json",
-        serde_json::to_string(&prog.effect).unwrap(),
-    )?;
-    std::fs::write(
-        "/tmp/is_effect_of_app.json",
-        serde_json::to_string(&prog.is_effect_of_app).unwrap(),
-    )?;
-    std::fs::write("/tmp/root.json", serde_json::to_string(&prog.root).unwrap())?;
-    std::fs::write(
-        "/tmp/parent.json",
-        serde_json::to_string(&prog.parent).unwrap(),
-    )?;
-    std::fs::write(
-        "/tmp/output.json",
-        serde_json::to_string(&prog.output).unwrap(),
-    )?;
-
-    Ok(prog.output)
+    Ok(prog.container)
 }
 
 fn process_args(
@@ -316,4 +542,89 @@ enum Edge {
     Variant,
     TraitApp,
     TraitEffect,
+}
+
+impl From<&Type> for Format {
+    fn from(type_: &Type) -> Self {
+        match type_ {
+            Type::ResolvedPath(path) => {
+                if let Some(args) = &path.args {
+                    match args.as_ref() {
+                        GenericArgs::AngleBracketed {
+                            args,
+                            constraints: _,
+                        } => {
+                            if path.name == "Option" {
+                                let format = match args[0] {
+                                    GenericArg::Type(ref t) => t.into(),
+                                    _ => todo!(),
+                                };
+                                Format::Option(Box::new(format))
+                            } else {
+                                Format::TypeName(path.name.clone())
+                            }
+                        }
+                        GenericArgs::Parenthesized {
+                            inputs: _,
+                            output: _,
+                        } => todo!(),
+                    }
+                } else {
+                    Format::TypeName(path.name.clone())
+                }
+            }
+            Type::DynTrait(_dyn_trait) => todo!(),
+            Type::Generic(_) => todo!(),
+            Type::Primitive(s) => match s.as_ref() {
+                "bool" => Format::Bool,
+                "char" => Format::Char,
+                "isize" => match std::mem::size_of::<isize>() {
+                    4 => Format::I32,
+                    8 => Format::I64,
+                    _ => panic!("unsupported isize size"),
+                },
+                "i8" => Format::I8,
+                "i16" => Format::I16,
+                "i32" => Format::I32,
+                "i64" => Format::I64,
+                "i128" => Format::I128,
+                "usize" => match std::mem::size_of::<usize>() {
+                    4 => Format::U32,
+                    8 => Format::U64,
+                    _ => panic!("unsupported usize size"),
+                },
+                "u8" => Format::U8,
+                "u16" => Format::U16,
+                "u32" => Format::U32,
+                "u64" => Format::U64,
+                "u128" => Format::U128,
+                s => panic!("need to implement primitive {s}"),
+            },
+            Type::FunctionPointer(_function_pointer) => todo!(),
+            Type::Tuple(_vec) => todo!(),
+            Type::Slice(_) => todo!(),
+            Type::Array { type_: _, len: _ } => todo!(),
+            Type::Pat {
+                type_: _,
+                __pat_unstable_do_not_use,
+            } => todo!(),
+            Type::ImplTrait(_vec) => todo!(),
+            Type::Infer => todo!(),
+            Type::RawPointer {
+                is_mutable: _,
+                type_: _,
+            } => todo!(),
+            Type::BorrowedRef {
+                lifetime: _,
+                is_mutable: _,
+                type_: _,
+            } => todo!(),
+            Type::QualifiedPath {
+                name: _,
+                args: _,
+                self_type: _,
+                trait_: _,
+            } => todo!(),
+        }
+    }
 }
