7c3d1de3f78f82980813179046d6b5694837e2dd
Stuart Harris <stuart.harris@red-badger.com>
2024-12-12 09:07:45 +0000

    load json from buffer, "full" test for examples

--- crux_cli/src/codegen/tests.rs
+++ crux_cli/src/codegen/tests.rs
@@ -1,217 +1,74 @@
-use std::fs::File;
-
 use anyhow::Result;
 use pretty_assertions::assert_eq;
+use rstest::rstest;
 use rustdoc_types::Crate;
 
 use crate::codegen::Registry;
 
-fn load(name: String) -> Result<Crate> {
+use super::rustdoc_json_path;
+
+fn load_rustdoc(name: String) -> Result<Crate> {
     Ok(match name.as_str() {
-        "cat_facts" => serde_json::from_str(include_str!("fixtures/cat_facts/rustdoc.json"))?,
-        "counter" => serde_json::from_str(include_str!("fixtures/counter/rustdoc.json"))?,
-        "notes" => serde_json::from_str(include_str!("fixtures/notes/rustdoc.json"))?,
-        "crux_core" => serde_json::from_str(include_str!("fixtures/crux_core.json"))?,
-        "crux_http" => serde_json::from_str(include_str!("fixtures/crux_http.json"))?,
-        "crux_kv" => serde_json::from_str(include_str!("fixtures/crux_kv.json"))?,
-        "crux_platform" => serde_json::from_str(include_str!("fixtures/crux_platform.json"))?,
-        "crux_time" => serde_json::from_str(include_str!("fixtures/crux_time.json"))?,
+        "bridge_echo" => {
+            serde_json::from_slice(include_bytes!("fixtures/bridge_echo/rustdoc.json"))?
+        }
+        "cat_facts" => serde_json::from_slice(include_bytes!("fixtures/cat_facts/rustdoc.json"))?,
+        "counter" => serde_json::from_slice(include_bytes!("fixtures/counter/rustdoc.json"))?,
+        "hello_world" => {
+            serde_json::from_slice(include_bytes!("fixtures/hello_world/rustdoc.json"))?
+        }
+        "notes" => serde_json::from_slice(include_bytes!("fixtures/notes/rustdoc.json"))?,
+        "simple_counter" => {
+            serde_json::from_slice(include_bytes!("fixtures/simple_counter/rustdoc.json"))?
+        }
+        "tap_to_pay" => serde_json::from_slice(include_bytes!("fixtures/tap_to_pay/rustdoc.json"))?,
+        "crux_core" => serde_json::from_slice(include_bytes!("fixtures/crux_core.json"))?,
+        "crux_http" => serde_json::from_slice(include_bytes!("fixtures/crux_http.json"))?,
+        "crux_kv" => serde_json::from_slice(include_bytes!("fixtures/crux_kv.json"))?,
+        "crux_platform" => serde_json::from_slice(include_bytes!("fixtures/crux_platform.json"))?,
+        "crux_time" => serde_json::from_slice(include_bytes!("fixtures/crux_time.json"))?,
+        "core" | "alloc" | "std" => {
+            let path = rustdoc_json_path()?.join(format!("{name}.json"));
+            let bytes = std::fs::read(&path)?;
+            serde_json::from_slice(&bytes)?
+        }
         _ => panic!("unknown crate {}", name),
     })
 }
 
-#[test]
-#[ignore = "not yet fully implemented"]
-fn cat_facts_json() {
-    let actual = super::run("cat_facts", true, load).unwrap();
-
-    let writer = File::create("fixtures-cat_facts-actual.json").unwrap();
-    serde_json::to_writer_pretty(writer, &actual).unwrap();
-    let expected: Registry = serde_json::from_str(include_str!("fixtures/cat_facts/expected.json"))
-        .expect("should deserialize");
-    assert_eq!(actual, expected);
-}
-
-#[test]
-#[ignore = "not yet fully implemented"]
-fn counter_json() {
-    let actual = super::run("counter", true, load).unwrap();
-
-    let writer = File::create("fixtures-counter-actual.json").unwrap();
-    serde_json::to_writer_pretty(writer, &actual).unwrap();
-    let expected: Registry = serde_json::from_str(include_str!("fixtures/counter/expected.json"))
-        .expect("should deserialize");
-    assert_eq!(actual, expected);
-}
-
-#[test]
-#[ignore = "not yet fully implemented"]
-fn notes_json() {
-    let actual = super::run("notes", true, load).unwrap();
-
-    let writer = File::create("fixtures-notes-actual.json").unwrap();
-    serde_json::to_writer_pretty(writer, &actual).unwrap();
-    let expected: Registry = serde_json::from_str(include_str!("fixtures/notes/expected.json"))
-        .expect("should deserialize");
-    assert_eq!(actual, expected);
-}
-
-#[test]
-fn bridge_echo() {
-    static RUSTDOC: &'static [u8] = include_bytes!("fixtures/bridge_echo_rustdoc.json");
-    let actual = super::run("bridge_echo", false, |_| {
-        Ok(serde_json::from_slice(RUSTDOC).unwrap())
+fn load_expected(name: &str) -> Result<Registry> {
+    Ok(match name {
+        "bridge_echo" => {
+            serde_json::from_slice(include_bytes!("fixtures/bridge_echo/expected.json"))?
+        }
+        "cat_facts" => serde_json::from_slice(include_bytes!("fixtures/cat_facts/expected.json"))?,
+        "counter" => serde_json::from_slice(include_bytes!("fixtures/counter/expected.json"))?,
+        "hello_world" => {
+            serde_json::from_slice(include_bytes!("fixtures/hello_world/expected.json"))?
+        }
+        "notes" => serde_json::from_slice(include_bytes!("fixtures/notes/expected.json"))?,
+        "simple_counter" => {
+            serde_json::from_slice(include_bytes!("fixtures/simple_counter/expected.json"))?
+        }
+        "tap_to_pay" => {
+            serde_json::from_slice(include_bytes!("fixtures/tap_to_pay/expected.json"))?
+        }
+        _ => panic!("unknown example {}", name),
     })
-    .unwrap();
-
-    insta::assert_debug_snapshot!(&actual, @r#"
-    {
-        "Effect": Enum(
-            {
-                0: Named {
-                    name: "Render",
-                    value: NewType(
-                        TypeName(
-                            "Operation",
-                        ),
-                    ),
-                },
-            },
-        ),
-        "Event": Enum(
-            {
-                0: Named {
-                    name: "Tick",
-                    value: Unit,
-                },
-                1: Named {
-                    name: "NewPeriod",
-                    value: Unit,
-                },
-            },
-        ),
-        "ViewModel": Struct(
-            [
-                Named {
-                    name: "count",
-                    value: U64,
-                },
-            ],
-        ),
-    }
-    "#);
 }
 
-#[test]
-fn cat_facts() {
-    static RUSTDOC: &'static [u8] = include_bytes!("fixtures/cat_facts/rustdoc.json");
-    let actual = super::run("cat_facts", false, |_| {
-        Ok(serde_json::from_slice(RUSTDOC).unwrap())
-    })
-    .unwrap();
+#[rstest]
+#[case::bridge_echo("bridge_echo")]
+#[case::cat_facts("cat_facts")]
+#[case::counter("counter")]
+#[case::hello_world("hello_world")]
+#[case::notes("notes")]
+#[case::simple_counter("simple_counter")]
+#[case::tap_to_pay("tap_to_pay")]
+#[ignore = "not yet fully implemented"]
+fn full(#[case] example: &str) {
+    let actual = super::run(example, true, load_rustdoc).unwrap();
+    let expected: Registry = load_expected(example).expect("should deserialize");
 
-    insta::assert_debug_snapshot!(&actual, @r#"
-    {
-        "CatImage": Struct(
-            [
-                Named {
-                    name: "href",
-                    value: Str,
-                },
-            ],
-        ),
-        "Effect": Enum(
-            {
-                0: Named {
-                    name: "Http",
-                    value: NewType(
-                        TypeName(
-                            "Operation",
-                        ),
-                    ),
-                },
-                1: Named {
-                    name: "KeyValue",
-                    value: NewType(
-                        TypeName(
-                            "Operation",
-                        ),
-                    ),
-                },
-                2: Named {
-                    name: "Platform",
-                    value: NewType(
-                        TypeName(
-                            "Operation",
-                        ),
-                    ),
-                },
-                3: Named {
-                    name: "Render",
-                    value: NewType(
-                        TypeName(
-                            "Operation",
-                        ),
-                    ),
-                },
-                4: Named {
-                    name: "Time",
-                    value: NewType(
-                        TypeName(
-                            "Operation",
-                        ),
-                    ),
-                },
-            },
-        ),
-        "Event": Enum(
-            {
-                0: Named {
-                    name: "None",
-                    value: Unit,
-                },
-                1: Named {
-                    name: "Clear",
-                    value: Unit,
-                },
-                2: Named {
-                    name: "Get",
-                    value: Unit,
-                },
-                3: Named {
-                    name: "Fetch",
-                    value: Unit,
-                },
-                4: Named {
-                    name: "GetPlatform",
-                    value: Unit,
-                },
-                5: Named {
-                    name: "Restore",
-                    value: Unit,
-                },
-            },
-        ),
-        "ViewModel": Struct(
-            [
-                Named {
-                    name: "fact",
-                    value: Str,
-                },
-                Named {
-                    name: "image",
-                    value: Option(
-                        TypeName(
-                            "CatImage",
-                        ),
-                    ),
-                },
-                Named {
-                    name: "platform",
-                    value: Str,
-                },
-            ],
-        ),
-    }
-    "#);
+    assert_eq!(actual, expected);
 }
