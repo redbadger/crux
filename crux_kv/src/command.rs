830160d16b92144a90d1d61fbe300669c7b55305
Viktor Charypar <charypar@gmail.com>
2025-01-22 17:03:49 +0000

    Return nicer types from crux_kv command builders

--- crux_kv/src/command.rs
+++ crux_kv/src/command.rs
@@ -4,13 +4,19 @@ use std::{future::Future, marker::PhantomData};
 
 use crux_core::{command::RequestBuilder, Command, Request};
 
-use super::{KeyValueOperation, KeyValueResult};
+use crate::error::KeyValueError;
+
+use super::KeyValueOperation;
 
 pub struct KeyValue<Effect, Event> {
     // Allow the impl to declare trait bounds once. Thanks rustc
     effect: PhantomData<Effect>,
     event: PhantomData<Event>,
 }
+
+type StatusResult = Result<bool, KeyValueError>;
+type DataResult = Result<Option<Vec<u8>>, KeyValueError>;
+type ListResult = Result<(Vec<String>, u64), KeyValueError>;
 
 impl<Effect, Event> KeyValue<Effect, Event>
 where
@@ -19,39 +25,44 @@ where
 {
     pub fn get(
         key: impl Into<String>,
-    ) -> RequestBuilder<Effect, Event, impl Future<Output = KeyValueResult>> {
+    ) -> RequestBuilder<Effect, Event, impl Future<Output = DataResult>> {
         Command::request_from_shell(KeyValueOperation::Get { key: key.into() })
+            .map(|kv_result| kv_result.unwrap_get())
     }
 
     pub fn set(
         key: impl Into<String>,
         value: Vec<u8>,
-    ) -> RequestBuilder<Effect, Event, impl Future<Output = KeyValueResult>> {
+    ) -> RequestBuilder<Effect, Event, impl Future<Output = DataResult>> {
         Command::request_from_shell(KeyValueOperation::Set {
             key: key.into(),
             value,
         })
+        .map(|kv_result| kv_result.unwrap_set())
     }
 
     pub fn delete(
         key: impl Into<String>,
-    ) -> RequestBuilder<Effect, Event, impl Future<Output = KeyValueResult>> {
+    ) -> RequestBuilder<Effect, Event, impl Future<Output = DataResult>> {
         Command::request_from_shell(KeyValueOperation::Delete { key: key.into() })
+            .map(|kv_result| kv_result.unwrap_delete())
     }
 
     pub fn exists(
         key: impl Into<String>,
-    ) -> RequestBuilder<Effect, Event, impl Future<Output = KeyValueResult>> {
+    ) -> RequestBuilder<Effect, Event, impl Future<Output = StatusResult>> {
         Command::request_from_shell(KeyValueOperation::Exists { key: key.into() })
+            .map(|kv_result| kv_result.unwrap_exists())
     }
 
     pub fn list_keys(
         prefix: impl Into<String>,
         cursor: u64,
-    ) -> RequestBuilder<Effect, Event, impl Future<Output = KeyValueResult>> {
+    ) -> RequestBuilder<Effect, Event, impl Future<Output = ListResult>> {
         Command::request_from_shell(KeyValueOperation::ListKeys {
             prefix: prefix.into(),
             cursor,
         })
+        .map(|kv_result| kv_result.unwrap_list_keys())
     }
 }
