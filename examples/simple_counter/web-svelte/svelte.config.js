ae4b672026ac203aade1268544e48498877d32a7
Stuart Harris <stuart.harris@red-badger.com>
2025-03-07 15:17:36 +0000

    pnpm deps and new svelte example with sveltekit

---
+++ examples/simple_counter/web-svelte/svelte.config.js
@@ -0,0 +1,21 @@
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-static';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	preprocess: vitePreprocess(),
+
+	kit: {
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
+	}
+};
+
+export default config;
