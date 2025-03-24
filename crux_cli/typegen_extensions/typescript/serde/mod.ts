e769c0aa6c56071fda1e023cfdb3dccbabb0ad16
Stuart Harris <stuart.harris@red-badger.com>
2025-03-24 11:50:36 +0000

    generate

---
+++ crux_cli/typegen_extensions/typescript/serde/mod.ts
@@ -0,0 +1,10 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates
+ * SPDX-License-Identifier: MIT OR Apache-2.0
+ */
+
+export * from "./types";
+export * from "./serializer";
+export * from "./deserializer";
+export * from "./binarySerializer";
+export * from "./binaryDeserializer";
