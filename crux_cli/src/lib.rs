49b96b5d2459941c0417108ca5050e0ea0294e17
Stuart Harris <stuart.harris@red-badger.com>
2025-04-03 08:51:46 +0100

    remove tokio

--- crux_cli/src/lib.rs
+++ crux_cli/src/lib.rs
@@ -6,4 +6,26 @@ pub mod doctor;
 mod template;
 mod workspace;
 
-pub use args::{Cli, CodegenArgs, Commands, DoctorArgs};
+use anyhow::Result;
+use args::{Cli, Commands, DoctorArgs};
+use clap::Parser;
+
+pub fn run() -> Result<()> {
+    env_logger::init();
+
+    let cli = Cli::parse();
+    match &cli.command {
+        Commands::Doctor(DoctorArgs {
+            fix: _,
+            include_source_code,
+            template_dir,
+            path,
+        }) => doctor::doctor(
+            template_dir,
+            path.as_deref(),
+            cli.verbose,
+            *include_source_code,
+        ),
+        Commands::Codegen(args) => codegen::codegen(args),
+    }
+}
