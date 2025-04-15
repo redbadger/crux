55bf5f000c8a037b40fcbd273f7b43b17bd86fb6
Stuart Harris <stuart.harris@red-badger.com>
2025-04-15 13:56:54 +0100

    cli tweaks

--- crux_cli/src/lib.rs
+++ crux_cli/src/lib.rs
@@ -2,10 +2,7 @@ mod args;
 pub mod bindgen;
 pub mod codegen;
 
-use std::fs;
-
-use anyhow::{Context as _, Result};
-use camino::Utf8Path;
+use anyhow::Result;
 use clap::Parser;
 
 pub use args::CodegenArgs;
@@ -19,20 +16,4 @@ pub fn run() -> Result<()> {
         Commands::Codegen(args) => codegen::codegen(args),
         Commands::Bindgen(args) => bindgen::bindgen(args),
     }
-}
-
-/// Load TOML from file if the file exists.
-fn load_toml_file(source: Option<&Utf8Path>) -> Result<Option<toml::value::Table>> {
-    if let Some(source) = source {
-        if source.exists() {
-            let contents =
-                fs::read_to_string(source).with_context(|| format!("read file: {:?}", source))?;
-            return Ok(Some(
-                toml::de::from_str(&contents)
-                    .with_context(|| format!("parse toml: {:?}", source))?,
-            ));
-        }
-    }
-
-    Ok(None)
 }
