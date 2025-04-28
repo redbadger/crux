227341470c02f2fc02d093843ef3e82c76e18e65
Kai DeLorenzo <git@seaoflaurels.com>
2025-04-28 16:11:41 -0500

    INotifyPropertyChanged for ViewModel and moved dotnet build to the C# project

--- examples/simple_counter/Windows/SimpleCounter/App.xaml.cs
+++ examples/simple_counter/Windows/SimpleCounter/App.xaml.cs
@@ -1,37 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Microsoft.UI.Xaml.Shapes;
-using Windows.ApplicationModel;
-using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+﻿using Microsoft.UI.Xaml;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace SimpleCounter
-{
+namespace SimpleCounter {
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
     /// </summary>
-    public partial class App : Application
-    {
+    public partial class App : Application {
         /// <summary>
         /// Initializes the singleton application object.  This is the first line of authored code
         /// executed, and as such is the logical equivalent of main() or WinMain().
         /// </summary>
-        public App()
-        {
+        public App() {
             this.InitializeComponent();
         }
 
@@ -39,8 +20,7 @@ namespace SimpleCounter
         /// Invoked when the application is launched.
         /// </summary>
         /// <param name="args">Details about the launch request and process.</param>
-        protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
-        {
+        protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args) {
             m_window = new MainWindow();
             m_window.Activate();
         }
--- examples/simple_counter/Windows/SimpleCounter/MainWindow.xaml.cs
+++ examples/simple_counter/Windows/SimpleCounter/MainWindow.xaml.cs
@@ -1,61 +1,31 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
 using SharedTypes;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using static System.Formats.Asn1.AsnWriter;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace SimpleCounter
-{
+namespace SimpleCounter {
     /// <summary>
     /// An empty window that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainWindow : Window
-    {
-        private readonly Core _core;
+    internal sealed partial class MainWindow : Window {
+        public Core Core { get; }
 
-        public MainWindow()
-        {
+        public MainWindow() {
             this.InitializeComponent();
-            _core = new Core();
-            UpdateUI();
+            Core = new Core();
         }
 
-        private void UpdateUI()
-        {
-            // Update the UI based on the ViewModel
-            CounterText.Text = _core.View.count;
+        private void ResetButton_Click(object sender, RoutedEventArgs e) {
+            Core.Update(new Event.Reset());
         }
 
-        private void ResetButton_Click(object sender, RoutedEventArgs e)
-        {
-            _core.Update(new Event.Reset());
-            UpdateUI();
+        private void IncrementButton_Click(object sender, RoutedEventArgs e) {
+            Core.Update(new Event.Increment());
         }
 
-        private void IncrementButton_Click(object sender, RoutedEventArgs e)
-        {
-            _core.Update(new Event.Increment());
-            UpdateUI();
-        }
-
-        private void DecrementButton_Click(object sender, RoutedEventArgs e)
-        {
-            _core.Update(new Event.Decrement());
-            UpdateUI();
+        private void DecrementButton_Click(object sender, RoutedEventArgs e) {
+            Core.Update(new Event.Decrement());
         }
     }
 }
