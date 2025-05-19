783589bf8990a384af881ceb2c242b20476f44de
Stuart Harris <stuart.harris@red-badger.com>
2025-05-19 13:42:34 +0100

    add pedantic to CI

--- scripts/build_and_test.fish
+++ scripts/build_and_test.fish
@@ -4,7 +4,7 @@ for dir in . examples/*
     echo $dir
     pushd "$dir"
     cargo fmt --all --check
-    cargo build --all-features
-    cargo nextest run --all-features
+    cargo build --all-features; or return 1
+    cargo nextest run --all-features; or return 1
     popd
 end
