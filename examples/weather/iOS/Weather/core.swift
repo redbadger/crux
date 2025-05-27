ac2f9ac4668cf37aedd58386cc7efeb086f66591
Bilal T <bilal.tariq@red-badger.com>
2025-05-27 16:12:39 +0100

    iOS setup

--- examples/weather/iOS/Weather/core.swift
+++ examples/weather/iOS/Weather/core.swift
@@ -7,8 +7,7 @@ class Core: ObservableObject {
     @Published var view: ViewModel
 
     init() {
-
-        view = try! .bincodeDeserialize(input: [UInt8](CatFacts.view()))
+        self.view = try! .bincodeDeserialize(input: [UInt8](Weather.view()))
     }
 
     func update(_ event: Event) {
@@ -23,7 +22,7 @@ class Core: ObservableObject {
     func processEffect(_ request: Request) {
         switch request.effect {
         case .render:
-            view = try! .bincodeDeserialize(input: [UInt8](CatFacts.view()))
+            view = try! .bincodeDeserialize(input: [UInt8](Weather.view()))
         case let .http(req):
             Task {
                 let response = try! await requestHttp(req).get()
@@ -40,30 +39,8 @@ class Core: ObservableObject {
                     processEffect(request)
                 }
             }
-        case .time:
-            let now = Date().timeIntervalSince1970;
-            let response = TimeResponse.now(instant: Instant(seconds: UInt64(now), nanos: 0))
             
-            let effects = [UInt8](handleResponse(request.id, Data(try! response.bincodeSerialize())))
-            
-            let requests: [Request] = try! .bincodeDeserialize(input: effects)
-            for request in requests {
-                processEffect(request)
-            }
-        case .platform:
-            let response = PlatformResponse(value: get_platform())
-            
-            let effects = [UInt8](handleResponse(request.id, Data(try! response.bincodeSerialize())))
-            
-            let requests: [Request] = try! .bincodeDeserialize(input: effects)
-            for request in requests {
-                processEffect(request)
-            }
         case .keyValue: ()
         }
     }
-}
-
-func get_platform() -> String {
-    return UIDevice.current.systemName + " " + UIDevice.current.systemVersion
 }
