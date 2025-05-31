ed02dff6b100156c7530c72e208c2e133debdfd6
Stuart Harris <stuart.harris@red-badger.com>
2025-05-31 15:31:04 +0100

    feat(crux_core): add `then_notify` and `build` methods to `*Builder`

--- crux_core/src/command/builder.rs
+++ crux_core/src/command/builder.rs
@@ -39,6 +39,13 @@ where
         let make_task = self.make_task;
         make_task(ctx)
     }
+
+    /// Convert the [`NotificationBuilder`] into a [`Command`] to use in an sync context
+    pub fn build(self) -> Command<Effect, Event> {
+        Command::new(|ctx| async move {
+            self.into_future(ctx.clone()).await;
+        })
+    }
 }
 
 impl<Effect, Event, Task> From<NotificationBuilder<Effect, Event, Task>> for Command<Effect, Event>
@@ -78,6 +85,101 @@ where
         F: FnOnce(T) -> U + Send + 'static,
     {
         RequestBuilder::new(|ctx| self.into_future(ctx.clone()).map(map))
+    }
+
+    /// Chain a [`NotificationBuilder`] to run after completion of this one,
+    /// passing the result to the provided closure `make_next_builder`.
+    ///
+    /// The returned value of the closure must be a `NotificationBuilder`, which
+    /// can represent the notification to be sent before the composed future
+    /// is finished.
+    ///
+    /// If you want to chain a request, use [`Self::then_request`] instead.
+    /// If you want to chain a subscription, use [`Self::then_stream`] instead.
+    ///
+    /// The closure `make_next_builder` is only run *after* successful completion
+    /// of the `self` future.
+    ///
+    /// Note that this function consumes the receiving `RequestBuilder`
+    /// and returns a [`NotificationBuilder`] that represents the composition.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use crux_core::{Command, Request};
+    /// # use crux_core::capability::Operation;
+    /// # use serde::{Deserialize, Serialize};
+    /// # #[derive(Debug, PartialEq, Clone, Serialize, Deserialize)]
+    /// # enum AnOperation {
+    /// #     Request(u8),
+    /// #     Notify,
+    /// # }
+    /// #
+    /// # #[derive(Debug, PartialEq, Deserialize)]
+    /// # enum AnOperationOutput {
+    /// #     Response(String),
+    /// # }
+    /// #
+    /// # impl Operation for AnOperation {
+    /// #     type Output = AnOperationOutput;
+    /// # }
+    /// #
+    /// # #[derive(Debug)]
+    /// # enum Effect {
+    /// #     AnEffect(Request<AnOperation>),
+    /// # }
+    /// #
+    /// # impl From<Request<AnOperation>> for Effect {
+    /// #     fn from(request: Request<AnOperation>) -> Self {
+    /// #         Self::AnEffect(request)
+    /// #     }
+    /// # }
+    /// #
+    /// # #[derive(Debug, PartialEq)]
+    /// # enum Event {
+    /// #     Response(AnOperationOutput),
+    /// # }
+    /// let mut cmd: Command<Effect, Event> =
+    ///     Command::request_from_shell(AnOperation::Request(10))
+    ///     .then_notify(|response| {
+    ///         let AnOperationOutput::Response(_response) = response else {
+    ///             panic!("Invalid output!")
+    ///         };
+    ///
+    ///         // possibly do something with the response
+    ///
+    ///         Command::notify_shell(AnOperation::Notify)
+    ///     })
+    ///     .build();
+    ///
+    /// let effect = cmd.effects().next().unwrap();
+    /// let Effect::AnEffect(mut request) = effect;
+    ///
+    /// assert_eq!(request.operation, AnOperation::Request(10));
+    ///
+    /// request
+    ///     .resolve(AnOperationOutput::Response("ten".to_string()))
+    ///     .expect("should work");
+    ///
+    /// assert!(cmd.events().next().is_none());
+    /// let effect = cmd.effects().next().unwrap();
+    /// let Effect::AnEffect(request) = effect;
+    ///
+    /// assert_eq!(request.operation, AnOperation::Notify);
+    /// assert!(cmd.is_done());
+    /// ```
+    pub fn then_notify<F, NextTask>(
+        self,
+        make_next_builder: F,
+    ) -> NotificationBuilder<Effect, Event, impl Future<Output = ()>>
+    where
+        F: FnOnce(T) -> NotificationBuilder<Effect, Event, NextTask> + Send + 'static,
+        NextTask: Future<Output = ()> + Send + 'static,
+    {
+        NotificationBuilder::new(|ctx| {
+            self.into_future(ctx.clone())
+                .then(|out| make_next_builder(out).into_future(ctx))
+        })
     }
 
     /// Chain another [`RequestBuilder`] to run after completion of this one,
@@ -259,7 +361,7 @@ where
         })
     }
 
-    /// Convert the request builder into a future to use in an async context
+    /// Convert the [`RequestBuilder`] into a future to use in an async context
     #[must_use]
     pub fn into_future(self, ctx: CommandContext<Effect, Event>) -> Task {
         let make_task = self.make_task;
@@ -275,6 +377,21 @@ where
         Command::new(|ctx| async move {
             let out = self.into_future(ctx.clone()).await;
             ctx.send_event(event(out));
+        })
+    }
+
+    /// Convert the [`RequestBuilder`] into a [`Command`] to use in an sync context
+    ///
+    /// Note: You might be looking for [`then_send`](Self::then_send)
+    /// instead, which will send the output back into the app with an event.
+    ///
+    /// The command created in this function will *ignore* the output
+    /// of the request so may not be very useful.
+    /// It might be useful when using a 3rd party capability and you don't
+    /// care about the request's response.
+    pub fn build(self) -> Command<Effect, Event> {
+        Command::new(|ctx| async move {
+            self.into_future(ctx.clone()).await;
         })
     }
 }
@@ -503,11 +620,29 @@ where
         })
     }
 
-    /// Convert the stream builder into a stream to use in an async context
+    /// Convert the [`StreamBuilder`] into a stream to use in an async context
     #[must_use]
     pub fn into_stream(self, ctx: CommandContext<Effect, Event>) -> Task {
         let make_stream = self.make_stream;
 
         make_stream(ctx)
+    }
+
+    /// Convert the [`StreamBuilder`] into a [`Command`] to use in an sync context
+    ///
+    /// Note: You might be looking for [`then_send`](Self::then_send)
+    /// instead, which will send each item in the stream back into the
+    /// app with an event.
+    ///
+    /// The command created in this function will *ignore* the output
+    /// of the stream so may not be very useful.
+    /// It may be useful when using a 3rd party capability and you don't
+    /// care about the stream output.
+    pub fn build(self) -> Command<Effect, Event> {
+        Command::new(|ctx| async move {
+            let mut stream = pin!(self.into_stream(ctx.clone()));
+
+            while (stream.next().await).is_some() {}
+        })
     }
 }
