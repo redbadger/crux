4b38231f10c91bb1fd6c9218082930cba5c3d403
Bilal T <bilal.tariq@red-badger.com>
2025-06-23 15:28:13 +0100

    Location api client

---
+++ examples/weather/shared/src/location/client.rs
@@ -0,0 +1,55 @@
+use crux_core::command::RequestBuilder;
+use crux_http::command::Http;
+use crux_http::protocol::HttpRequest;
+use serde::Serialize;
+
+use crate::config::API_KEY;
+use crate::{Effect, GeocodingQueryString, GeocodingResponse, GEOCODING_URL};
+
+#[derive(Serialize)]
+pub struct CurrentQueryString {
+    pub lat: String,
+    pub lon: String,
+    pub units: &'static str,
+    pub appid: String,
+}
+
+pub struct LocationApi;
+
+impl LocationApi {
+    /// Build an `HttpRequest` for testing purposes
+    pub fn build(query: String) -> HttpRequest {
+        HttpRequest::get(GEOCODING_URL)
+            .query(&GeocodingQueryString {
+                q: query.to_string(),
+                limit: "5",
+                appid: API_KEY.clone(),
+            })
+            .expect("could not serialize query string")
+            .build()
+    }
+
+    /// Fetch current weather for a specific location
+    pub fn fetch<Event>(
+        query: String,
+    ) -> RequestBuilder<
+        Effect,
+        Event,
+        impl std::future::Future<
+            Output = Result<crux_http::Response<Vec<GeocodingResponse>>, crux_http::HttpError>,
+        >,
+    >
+    where
+        Event: Send + 'static,
+    {
+        Http::get(GEOCODING_URL)
+            .expect_json()
+            .query(&GeocodingQueryString {
+                q: query.to_string(),
+                limit: "5",
+                appid: API_KEY.clone(),
+            })
+            .expect("could not serialize query string")
+            .build()
+    }
+}
