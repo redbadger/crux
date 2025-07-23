4c38d8d8f3aa9462cc7dd2b806cbe3a88b8a7ef7
Stuart Harris <stuart.harris@red-badger.com>
2025-07-23 08:40:35 +0100

    chore(counter_next): remove shared types

---
+++ examples/counter-next/build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -eux
+
+pushd shared
+cargo swift package --name Shared --platforms ios
+pushd generated
+# remove stuff we don't need
+rm -rf headers sources *.swift *.h *.modulemap
+popd
+popd
+
+cargo build --package shared
+cargo run --package shared --bin codegen --features cli,facet_typegen
