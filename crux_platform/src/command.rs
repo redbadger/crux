84b1ad78cc895469eaab75544745a807623b5065
Viktor Charypar <charypar@gmail.com>
2025-10-26 20:53:53 +0000

    Update crux_platform

--- crux_platform/src/command.rs
+++ crux_platform/src/command.rs
@@ -1,22 +1,3 @@
-use std::{future::Future, marker::PhantomData};
-
-use crux_core::{Command, Request, command::RequestBuilder};
-
-use crate::{PlatformRequest, PlatformResponse};
-
-pub struct Platform<Effect, Event> {
-    effect: PhantomData<Effect>,
-    event: PhantomData<Event>,
-}
-
-impl<Effect, Event> Platform<Effect, Event>
-where
-    Effect: From<Request<PlatformRequest>> + Send + 'static,
-    Event: Send + 'static,
-{
-    /// Get the platform of the shell
-    #[must_use]
-    pub fn get() -> RequestBuilder<Effect, Event, impl Future<Output = PlatformResponse>> {
-        Command::request_from_shell(PlatformRequest)
-    }
-}
+// For backwards compatibility
+#[deprecated(since = "0.17.9", note = "import directly from crate root")]
+pub use crate::Platform;
