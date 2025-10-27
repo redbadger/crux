0bfd7db2ab8b76c0f31ccb533bab0a828f7628c1
Viktor Charypar <charypar@gmail.com>
2025-10-27 00:12:40 +0000

    Align depreceation messages

--- crux_platform/src/command.rs
+++ crux_platform/src/command.rs
@@ -1,3 +1,3 @@
 // For backwards compatibility
-#[deprecated(since = "0.17.9", note = "import directly from crate root")]
+#[deprecated(since = "0.17.9", note = "Import directly from crate root")]
 pub use crate::Platform;
--- crux_time/src/command.rs
+++ crux_time/src/command.rs
@@ -1,2 +1,2 @@
-#[deprecated()]
+#[deprecated(since = "0.15.0", note = "Import directly from crate root")]
 pub use crate::{CompletedTimerHandle, Time, TimerHandle, TimerOutcome};
