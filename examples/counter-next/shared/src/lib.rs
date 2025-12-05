6bea07676c4c896eabef97645cbaa595ebaf5d60
Stuart Harris <stuart.harris@red-badger.com>
2025-12-05 17:42:03 +0000

    revert to uniffi 0.29.4

--- examples/counter-next/shared/src/lib.rs
+++ examples/counter-next/shared/src/lib.rs
@@ -12,8 +12,8 @@ pub use crux_core::Core;
 
 #[cfg(feature = "uniffi")]
 const _: () = assert!(
-    uniffi::check_compatible_version("0.30.0"),
-    "please use uniffi v0.30.0"
+    uniffi::check_compatible_version("0.29.4"),
+    "please use uniffi v0.29.4"
 );
 #[cfg(feature = "uniffi")]
 uniffi::setup_scaffolding!();
