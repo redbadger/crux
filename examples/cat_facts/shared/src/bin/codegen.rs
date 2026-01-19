ee55b292819027c1709d4acb0aa77ff3d0ca16e4
Martin Geisler <martin@geisler.net>
2026-01-19 23:07:56 +0100

    refactor: streamline codegen examples
    This refactors some very repetitive/duplicate code in the example
    codegen files.
    
    I feel it’s important to make the examples as simple and “crisp” as
    possible, especially since I’ve seen people new to Rust copy this code
    directly into our internal code base.

--- examples/cat_facts/shared/src/bin/codegen.rs
+++ examples/cat_facts/shared/src/bin/codegen.rs
@@ -32,41 +32,35 @@ fn main() -> Result<()> {
 
     let typegen_app = TypeRegistry::new().register_app::<CatFacts>()?.build()?;
 
+    let name = match args.language {
+        Language::Swift => "App",
+        Language::Kotlin => "com.crux.example.cat_facts",
+        Language::Typescript => "app",
+    };
+    let config = Config::builder(name, &args.output_dir)
+        .add_extensions()
+        .add_runtimes()
+        .build();
+
     match args.language {
         Language::Swift => {
             info!("Typegen for Swift");
-            typegen_app.swift(
-                &Config::builder("App", &args.output_dir)
-                    .add_extensions()
-                    .add_runtimes()
-                    .build(),
-            )?;
+            typegen_app.swift(&config)?;
         }
         Language::Kotlin => {
             info!("Typegen for Kotlin");
-            typegen_app.kotlin(
-                &Config::builder("com.crux.example.cat_facts", &args.output_dir)
-                    .add_extensions()
-                    .add_runtimes()
-                    .build(),
-            )?;
+            typegen_app.kotlin(&config)?;
 
             info!("Bindgen for Kotlin");
-            bindgen(
-                &BindgenArgsBuilder::default()
-                    .crate_name(env!("CARGO_PKG_NAME").to_string())
-                    .kotlin(&args.output_dir)
-                    .build()?,
-            )?;
+            let bindgen_args = BindgenArgsBuilder::default()
+                .crate_name(env!("CARGO_PKG_NAME").to_string())
+                .kotlin(&args.output_dir)
+                .build()?;
+            bindgen(&bindgen_args)?;
         }
         Language::Typescript => {
             info!("Typegen for TypeScript");
-            typegen_app.typescript(
-                &Config::builder("app", &args.output_dir)
-                    .add_extensions()
-                    .add_runtimes()
-                    .build(),
-            )?;
+            typegen_app.typescript(&config)?;
         }
     }
 
