cb572e45d8cde1e5a0c2b275805aff592abe2b72
Stuart Harris <stuart.harris@red-badger.com>
2026-06-02 16:52:43 +0100

    gate ffi (default) so that leptos shell can remove it

--- examples/counter-routing/shared/src/lib.rs
+++ examples/counter-routing/shared/src/lib.rs
@@ -1,8 +1,9 @@
 #![allow(clippy::unsafe_derive_deserialize)]
 pub mod app;
 mod capabilities;
+#[cfg(feature = "ffi")]
 mod ffi;
-#[cfg(not(target_family = "wasm"))]
+#[cfg(all(feature = "ffi", not(target_family = "wasm")))]
 mod rng_handler;
 
 pub use crux_http as http;
@@ -11,4 +12,5 @@ pub use app::*;
 pub use capabilities::{RandomNumber, RandomNumberRequest, sse};
 pub use crux_core::Core;
 
+#[cfg(feature = "ffi")]
 pub use ffi::CoreFFI;
